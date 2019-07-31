bbc0db0748f4fff08319f0fb1b0d16653b8ab6db
Cormac Garvey <cogarvey@microsoft.com>
2019-07-31 16:14:10 -0600

    Added SHARE_SCRATCH and started beegfs client service

--- scripts/beegfsc.sh
+++ scripts/beegfsc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 MGMT_HOSTNAME=$1
+SHARE_SCRATCH=/beegfs
 
 yum install -y beegfs-client beegfs-helperd beegfs-utils
 
 sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
-sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
+echo "$SHARE_SCRATCH /etc/beegfs/beegfs-client.conf" > /etc/beegfs/beegfs-mounts.conf
 
 systemctl daemon-reload
 systemctl enable beegfs-helperd.service
 systemctl enable beegfs-client.service
+systemctl start beegfs-client.service
