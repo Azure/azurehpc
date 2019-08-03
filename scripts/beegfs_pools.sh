6a6f31a59227e2662f8a28b334cc7f8e2b5d4d53
Cormac Garvey <cogarvey@microsoft.com>
2019-08-02 21:44:28 -0600

    Removed debug echo prints

--- scripts/beegfs_pools.sh
+++ scripts/beegfs_pools.sh
@@ -4,11 +4,8 @@
 num_hdd_targets=$1
 BEEGFS_SHARE=/beegfs
 
-#systemctl restart beegfs-client
 cnt=$((num_hdd_targets*2))
-echo "cnt=$cnt"
 hdd_target_str=$(seq -s, 1 2 $cnt)
-echo "hdd_target_str=$hdd_target_str"
 beegfs-ctl --addstoragepool --desc="hdd_pool" --targets=$hdd_target_str
 HDD_POOL_DIR=${BEEGFS_SHARE}/hdd_pool
 mkdir $HDD_POOL_DIR
