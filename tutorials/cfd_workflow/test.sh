55fd9b80eedae2b7d053fc11a11c6814f7d4c850
Xavier Pillons <xpillons@microsoft.com>
2019-08-08 16:24:04 -0500

    added

---
+++ tutorials/cfd_workflow/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+qstat
+pbsnodes -avS
+nodes=$(pbsnodes -a -F dsv -L -S | wc -l)
+if [ "$nodes" = 0 ]; then
+    echo "Error - No nodes in PBS"
+    exit 1
+fi
+df -h
+
+qsub -l select=2:ncpus=60:mpiprocs=60,place=scatter:excl -N OF_motorbike_2m $HOME/apps/openfoam_org/motorbike_2m.sh
+
+qstat -aw
+
+jobs=$(qstat | wc -l)
+while [ "$jobs" != "0" ]; do
+    qstat
+    jobs=$(qstat | wc -l)
+    sleep 30
+done
+
+cat *.log
