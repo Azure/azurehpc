3f285040e555eed425e132fac929e6ee658b662a
Xavier Pillons <xpillons@microsoft.com>
2019-09-05 17:13:16 +0200

    rename LSF_TOP to LSF_DIR to avoid collision

--- scripts/lsfnode.sh
+++ scripts/lsfnode.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+LSF_DIR=/apps/lsf
 
-LSF_TOP=/apps/lsf
-CLUSTERNAME=azhpc
-
-source $LSF_TOP/conf/profile.lsf 
-cd $LSF_TOP/10.1/install
-sudo ./hostsetup --top="$LSF_TOP" --boot="y" --dynamic --start="y"
+source $LSF_DIR/conf/profile.lsf 
+cd $LSF_DIR/10.1/install
+sudo ./hostsetup --top="$LSF_DIR" --boot="y" --dynamic --start="y"
 
 ps -aux | grep lsf
 
