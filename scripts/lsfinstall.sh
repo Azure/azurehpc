66b801e58860cf0788b93924ecd17994789570c1
Xavier Pillons <xpillons@microsoft.com>
2019-09-05 14:48:14 +0200

    add dynamic hosts

--- scripts/lsfinstall.sh
+++ scripts/lsfinstall.sh
@@ -4,6 +4,8 @@ LSF_INSTALL_DIR=$LSF_DOWNLOAD_DIR/lsf10.1_lsfinstall
 LSF_INSTALL_CONFIG=$LSF_INSTALL_DIR/lsf.install.config
 LSF_TOP=/apps/lsf
 LSF_CONF=$LSF_TOP/conf/lsf.conf
+CLUSTERNAME=azhpc
+LSFADMIN=hpcadmin
 
 # Install dependencies
 yum -y install jre
@@ -13,8 +15,8 @@ cp $LSF_INSTALL_DIR/install.config $LSF_INSTALL_CONFIG
 
 echo "Updating install.config"
 sed -i 's|# LSF_TOP="/usr/share/lsf"|LSF_TOP="'$LSF_TOP'"|g' $LSF_INSTALL_CONFIG
-sed -i 's|# LSF_ADMINS="lsfadmin user1 user2"|LSF_ADMINS="hpcadmin"|g' $LSF_INSTALL_CONFIG
-sed -i 's|# LSF_CLUSTER_NAME="cluster1"|LSF_CLUSTER_NAME="azurehpc"|g' $LSF_INSTALL_CONFIG
+sed -i 's|# LSF_ADMINS="lsfadmin user1 user2"|LSF_ADMINS="'$LSFADMIN'"|g' $LSF_INSTALL_CONFIG
+sed -i 's|# LSF_CLUSTER_NAME="cluster1"|LSF_CLUSTER_NAME="'$CLUSTERNAME'"|g' $LSF_INSTALL_CONFIG
 sed -i 's|# LSF_MASTER_LIST="hostm hosta hostc"|LSF_MASTER_LIST="'$(hostname)'"|g' $LSF_INSTALL_CONFIG
 sed -i 's|# LSF_ENTITLEMENT_FILE="/usr/share/lsf/lsf_distrib/lsf_std_entitlement.dat"|LSF_ENTITLEMENT_FILE="'$LSF_DOWNLOAD_DIR'/lsf_std_entitlement.dat"|g' $LSF_INSTALL_CONFIG
 sed -i 's|# LSF_TARDIR="/usr/share/lsf_distrib/"|LSF_TARDIR="'$LSF_DOWNLOAD_DIR'"|g' $LSF_INSTALL_CONFIG
@@ -40,7 +42,7 @@ tar zxvf $LSF_DOWNLOAD_DIR/lsf10.1_linux2.6-glibc2.3-x86_64-509238.tar.Z
 
 
 cat << EOF >> /etc/lsf.sudoers
-LSF_STARTUP_USERS="hpcadmin"
+LSF_STARTUP_USERS="$LSFADMIN"
 LSF_STARTUP_PATH="$LSF_TOP/10.1/linux2.6-glibc2.3-x86_64/etc"
 EOF
 
@@ -56,7 +58,7 @@ chown -R root:root $LSF_TOP
 chmod 4755 $LSF_TOP/10.1/linux2.6-glibc2.3-x86_64/bin/lsadmin
 chmod 4755 $LSF_TOP/10.1/linux2.6-glibc2.3-x86_64/bin/badmin
 
-chown -R hpcadmin:hpcadmin $LSF_TOP/work/azurehpc/
+chown -R $LSFADMIN:$LSFADMIN $LSF_TOP/work/$CLUSTERNAME/
 
 cat << EOF >> /etc/security/limits.conf
 * soft nofile 65535
@@ -74,3 +76,16 @@ LSF_STRIP_DOMAIN=.$master_domain
 LSF_DYNAMIC_HOST_TIMEOUT=10m
 EOF
 
+source $LSF_TOP/conf/profile.lsf 
+# get info from setup
+lsid
+
+# List clusters
+lsclusters
+
+# List queues
+bqueues
+
+# List hosts
+bhosts
+lshosts
---
+++ scripts/lsfnode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LSF_TOP=/apps/lsf
+CLUSTERNAME=azhpc
+
+source $LSF_TOP/conf/profile.lsf 
+cd $LSF_TOP/10.1/install
+./hostsetup --top="$LSF_TOP" --boot="y" --dynamic --start="y"
+
+ps -aux | grep lsf
+
