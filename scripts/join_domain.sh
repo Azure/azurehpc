2403963cb69dc9ec207eaff0e3f5006d97363733
hugo meiland <hugo.meiland@microsoft.com>
2019-09-06 08:35:35 +0200

    removed hardcoded nameserver

--- scripts/join_domain.sh
+++ scripts/join_domain.sh
@@ -3,43 +3,35 @@
 ADMIN_DOMAIN=$1
 ADMIN_NAME=$2
 ADMIN_PASSWORD=$3
+AD_SERVER=$4
 
-echo $1 $2 $3
+echo $1 $2 $3 $4
 
 yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
 
 systemctl restart dbus
 systemctl restart systemd-logind
 
-#NAMESERVER=$(jetpack config adjoin.ad.dns1)
-NAMESERVER=10.2.1.4
-if [ ! -f /etc/resolv.conf.bak ]; then
-    cp /etc/resolv.conf /etc/resolv.conf.bak
-fi
-if grep -q "nameserver $NAMESERVER" /etc/resolv.conf ; then
-    echo "resolv.conf already changed"
-else
-    echo "nameserver $NAMESERVER" >> /etc/resolv.conf.tmp
-    cat /etc/resolv.conf >> /etc/resolv.conf.tmp
-    mv /etc/resolv.conf.tmp /etc/resolv.conf
-fi
+NAMESERVER=`getent ahosts $AD_SERVER | awk '{print $1;exit}'`
+echo "supersede domain-name-servers ${NAMESERVER};" > /etc/dhcp/dhclient.conf
+echo "append domain-name-servers 168.63.129.16;" >> /etc/dhcp/dhclient.conf
+systemctl restart NetworkManager
 
-#ADMIN_DOMAIN=$(jetpack config adjoin.ad.domain)
-#ADMIN_DOMAIN=MyDomain.local
-#ADMIN_NAME=$(jetpack config adjoin.ad.admin.name)
-#ADMIN_NAME=hpcadmin
-#ADMIN_PASSWORD=$(jetpack config adjoin.ad.admin.password)
-#ADMIN_PASSWORD=admin_1234567
+sleep 10
+
 echo $ADMIN_PASSWORD| realm join -U $ADMIN_NAME $ADMIN_DOMAIN --verbose
 
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-service sshd restart
-
+systemctl restart sshd
 
 sed -i 's@override_homedir.*@override_homedir = /share/home/%u@' /etc/sssd/sssd.conf
 sed -i 's@fallback_homedir.*@fallback_homedir = /share/home/%u@' /etc/sssd/sssd.conf
 sed -i 's@use_fully_qualified_names.*@use_fully_qualified_names = False@' /etc/sssd/sssd.conf
-service sssd restart
+systemctl restart sssd
 
-
+cat <<EOF >/etc/ssh/ssh_config
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+EOF
