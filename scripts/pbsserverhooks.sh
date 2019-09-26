518f4a295ebbc5f375396991100d3378551acd18
JonShelley <jshelley.menan@gmail.com>
2019-09-26 17:17:42 +0000

    fixed quoting issue

--- scripts/pbsserverhooks.sh
+++ scripts/pbsserverhooks.sh
@@ -4,8 +4,8 @@
 HOOKS_DIR=~/azhpc_install_config/scripts/pbshooks
 sudo /opt/pbs/bin/qmgr -c 'create hook setup_jobdir'
 sudo /opt/pbs/bin/qmgr -c 'set hook setup_jobdir event="execjob_begin,execjob_end"'
-sudo /opt/pbs/bin/qmgr -c 'import hook setup_jobdir application/x-python default $HOOKS_DIR/setup_jobdir.py'
+sudo /opt/pbs/bin/qmgr -c "import hook setup_jobdir application/x-python default $HOOKS_DIR/setup_jobdir.py"
 
 sudo /opt/pbs/bin/qmgr -c 'create hook stop_wa'
 sudo /opt/pbs/bin/qmgr -c 'set hook stop_wa event="execjob_begin,execjob_end"'
-sudo /opt/pbs/bin/qmgr -c 'import hook stop_wa application/x-python default $HOOKS_DIR/stop_waagent.py'
+sudo /opt/pbs/bin/qmgr -c "import hook stop_wa application/x-python default $HOOKS_DIR/stop_waagent.py"
