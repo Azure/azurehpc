99964edf5301f5cbd6474cb95e937300ac2cc7c5
Xavier Pillons <xpillons@microsoft.com>
2019-09-27 12:17:36 +0200

    remove HB/HC sku dependencies

---
+++ apps/ior/mdtest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+FILESYSTEM=$1
+SHARED_APP=${2:-/apps}
+
+DIRECTORY=${FILESYSTEM}/testing
+NUMFILES=10000
+
+export MODULEPATH=${SHARED_APP}/modulefiles:$MODULEPATH
+module load gcc-8.2.0
+# ior environment contains mdtest also.
+module load ior
+
+AZHPC_VMSIZE=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2018-10-01" | jq -r '.compute.vmSize')
+export AZHPC_VMSIZE=${AZHPC_VMSIZE,,}
+
+case $AZHPC_VMSIZE:
+    standard_hb60rs)
+    standard_hc44rs)
+        module load mpi/mpich-3.3
+        PKEY=$(grep -v -e 0000 -e 0x7fff --no-filename /sys/class/infiniband/mlx5_0/ports/1/pkeys/*)
+        PKEY=${PKEY/0x8/0x0}
+        mpi_options="-env UCX_IB_PKEY=$PKEY"
+    ;;
+
+    default)
+        module load mpi/mpich-3.2-x86_64
+    ;;
+esac
+
+# Metadata test
+mpirun  -bind-to hwthread $mpi_options mdtest -n $NUMFILES -d $DIRECTORY -i 3 -u
