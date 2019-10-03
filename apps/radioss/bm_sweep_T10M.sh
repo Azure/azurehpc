d4c5f0b90c324bb2556caab47502cc0da565eadb
JonShelley <jshelley.menan@gmail.com>
2019-10-03 19:31:06 +0000

    updated sweep script

--- apps/radioss/bm_sweep_T10M.sh
+++ apps/radioss/bm_sweep_T10M.sh
@@ -2,12 +2,12 @@
 
 for x in 1 2 4 8 16 32 64 96 128; do
     for y in 36 40 44; do
-        qsub -l select=$x:ncpus=$y:mpiprocs=$y:ompthreads=1,place=scatter:excl -v LIC_SRV=headnode  apps/radioss/run_T10M.pbs
+        qsub -N bm_${x}n_${y}_1t -l select=$x:ncpus=$y:mpiprocs=$y:ompthreads=1,place=scatter:excl -v LIC_SRV=headnode  ~/apps/radioss/run_T10M.pbs
         if [ "$x" -gt 8 ]; then
             z1=$(($y/2))
-            qsub -l select=$x:ncpus=$z1:mpiprocs=$z1:ompthreads=2,place=scatter:excl -v LIC_SRV=headnode  apps/radioss/run_T10M.pbs 
+            qsub -N bm_${x}n_${z1}_2t -l select=$x:ncpus=$z1:mpiprocs=$z1:ompthreads=2,place=scatter:excl -v LIC_SRV=headnode  ~/apps/radioss/run_T10M.pbs 
             z2=$(($y/4))
-            qsub -l select=$x:ncpus=$z2:mpiprocs=$z2:ompthreads=4,place=scatter:excl -v LIC_SRV=headnode  apps/radioss/run_T10M.pbs
+            qsub -N bm_${x}n_${z2}_4t -l select=$x:ncpus=$z2:mpiprocs=$z2:ompthreads=4,place=scatter:excl -v LIC_SRV=headnode  ~/apps/radioss/run_T10M.pbs
         fi
     done
 done
