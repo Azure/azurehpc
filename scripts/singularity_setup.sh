ccd35e06c7b47494f93e28787acd82b4a5b7e161
hugo meiland <hugo.meiland@microsoft.com>
2019-10-09 10:02:06 -0700

    adding singularity script and example

---
+++ scripts/singularity_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+acr_user=$1
+acr_repo=$2
+acr_pass=$3
+
+yum --releasever=7.6.1810 -y install singularity
+
+wget https://github.com/deislabs/oras/releases/download/v0.7.0/oras_0.7.0_linux_amd64.tar.gz -O - | tar -C /usr/local/bin -zxvf - oras
+
+cat <<EOF >>/etc/bashrc
+export SINGULARITY_DOCKER_USERNAME=$acr_repo
+export SINGULARITY_DOCKER_PASSWORD="$acr_pass"
+EOF
