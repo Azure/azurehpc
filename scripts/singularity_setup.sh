c172310ef57066018c0fe830cd68d79604aa33bd
Xavier Pillons <xpillons@microsoft.com>
2019-10-16 14:43:07 +0200

    use /mnt/resource for cache and tmp

--- scripts/singularity_setup.sh
+++ scripts/singularity_setup.sh
@@ -9,7 +9,10 @@ yum --releasever=$RELEASE -y install singularity
 
 wget -q https://github.com/deislabs/oras/releases/download/v0.7.0/oras_0.7.0_linux_amd64.tar.gz -O - | tar -C /usr/local/bin -zxvf - oras
 
+
 cat <<EOF >>/etc/profile.d/azhpc_singularity.sh
 export SINGULARITY_DOCKER_USERNAME=$acr_repo
 export SINGULARITY_DOCKER_PASSWORD="$acr_pass"
+export SINGULARITY_CACHEDIR=/mnt/resource
+export SINGULARITY_TMPDIR=/mnt/resource
 EOF
