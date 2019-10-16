5b794d14977e3865785defb8c48578937983b79e
Xavier Pillons <xpillons@microsoft.com>
2019-10-16 12:28:34 +0200

    fix singularity scripts and config

--- scripts/singularity_setup.sh
+++ scripts/singularity_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 acr_repo=$1
 acr_pass=$2
 
-yum --releasever=7.6.1810 -y install singularity
+RELEASE=$(cat /etc/redhat-release | cut -d' ' -f4)
+echo $RELEASE
 
-wget https://github.com/deislabs/oras/releases/download/v0.7.0/oras_0.7.0_linux_amd64.tar.gz -O - | tar -C /usr/local/bin -zxvf - oras
+yum --releasever=$RELEASE -y install singularity
 
-cat <<EOF >>/etc/bashrc
+wget -q https://github.com/deislabs/oras/releases/download/v0.7.0/oras_0.7.0_linux_amd64.tar.gz -O - | tar -C /usr/local/bin -zxvf - oras
+
+cat <<EOF >>/etc/profile.d/azhpc_singularity.sh
 export SINGULARITY_DOCKER_USERNAME=$acr_repo
 export SINGULARITY_DOCKER_PASSWORD="$acr_pass"
 EOF
