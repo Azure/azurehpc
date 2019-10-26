51ce89d576cb04d7b9a104a98b61b883d3bbf5ff
Cormac Garvey <cogarvey@microsoft.com>
2019-10-26 12:26:04 -0600

    Added default for DATA_INSTALL_DIR

--- apps/intersect/install_case_intersect_2018.2.sh
+++ apps/intersect/install_case_intersect_2018.2.sh
@@ -3,7 +3,7 @@
 DATA_TAR_SAS_URL=/path/to/casedata.tar.tgz
 DATA_TAR=BO_192_192_28.tgz
 
-DATA_INSTALL_DIR=/data
+DATA_INSTALL_DIR=${DATA_INSTALL_DIR:-/data}
 
 pushd $DATA_INSTALL_DIR
 if [ ! -f ${DATA_INSTALL_DIR}/${DATA_TAR} ]; then
