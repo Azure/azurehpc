91e88d43eadf46a463f7b458fc6cafbac8cb1b8f
Cormac Garvey <cogarvey@microsoft.com>
2019-10-31 15:58:40 -0600

    Changed TAR to TGZ

--- apps/intersect/install_case_intersect_2018.2.sh
+++ apps/intersect/install_case_intersect_2018.2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-#Set environmental variable DATA_TAR_SAS_URL
-#DATA_TAR_SAS_URL=/path/to/casedata.tar.tgz
+#Set environmental variable DATA_TGZ_SAS_URL
+#DATA_TGZ_SAS_URL=/url_path/to/casedata.tgz
 
-if [[ $IX_DATA_TAR_SAS_URL =~ .*\/(.+)\? ]]; then
-   IX_DATA_TAR=${BASH_REMATCH[1]}
+if [[ $IX_DATA_TGZ_SAS_URL =~ .*\/(.+)\? ]]; then
+   IX_DATA_TGZ=${BASH_REMATCH[1]}
 fi
 
 IX_DATA_INSTALL_DIR=${IX_DATA_INSTALL_DIR:-/data}
 
 pushd $IX_DATA_INSTALL_DIR
-if [ ! -f ${IX_DATA_INSTALL_DIR}/${IX_DATA_TAR} ]; then
-wget -O ${IX_DATA_TAR} "$IX_DATA_TAR_SAS_URL"
+if [ ! -f ${IX_DATA_INSTALL_DIR}/${IX_DATA_TGZ} ]; then
+wget -O ${IX_DATA_TGZ} "$IX_DATA_TGZ_SAS_URL"
 fi
 popd
