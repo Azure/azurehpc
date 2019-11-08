33af37ae1333698ec9d80e679a219b83da15add5
Xavier Pillons <xpillons@microsoft.com>
2019-11-08 13:09:44 +0100

    disable openlogic as it failed too often

--- scripts/update_kernel.sh
+++ scripts/update_kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 RELEASE=$(cat /etc/redhat-release | cut -d' ' -f4)
 echo $RELEASE
-yum --releasever=$RELEASE install -y kernel kernel-tools kernel-headers kernel-devel
+yum --releasever=$RELEASE --disablerepo=openlogic install -y kernel kernel-tools kernel-headers kernel-devel
