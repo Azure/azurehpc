2e26dc9128e77736285c3122443597344ab45028
JonShelley <jshelley.menan@gmail.com>
2019-12-03 21:19:29 +0000

    Added fixes so that users could run azhpc-build on partially completed builds without the scripts erroring out on compute nodes

--- scripts/pbsclient.sh
+++ scripts/pbsclient.sh
@@ -3,13 +3,17 @@
 # arg: $1 = pbs_server
 pbs_server=$1
 
-sudo yum install -y pbspro-execution-19.1.1-0.x86_64.rpm
+if [ "$(rpm -qa pbspro-execution)" = "" ];then
+    sudo yum install -y pbspro-execution-19.1.1-0.x86_64.rpm
 
-sudo sed -i "s/CHANGE_THIS_TO_PBS_PRO_SERVER_HOSTNAME/${pbs_server}/g" /etc/pbs.conf
-sudo sed -i "s/CHANGE_THIS_TO_PBS_PRO_SERVER_HOSTNAME/${pbs_server}/g" /var/spool/pbs/mom_priv/config
-sudo sed -i "s/^if /#if /g" /opt/pbs/lib/init.d/limits.pbs_mom
-sudo sed -i "s/^fi/#fi /g" /opt/pbs/lib/init.d/limits.pbs_mom
-sudo systemctl enable pbs
-sudo systemctl start pbs
+    sudo sed -i "s/CHANGE_THIS_TO_PBS_PRO_SERVER_HOSTNAME/${pbs_server}/g" /etc/pbs.conf
+    sudo sed -i "s/CHANGE_THIS_TO_PBS_PRO_SERVER_HOSTNAME/${pbs_server}/g" /var/spool/pbs/mom_priv/config
+    sudo sed -i "s/^if /#if /g" /opt/pbs/lib/init.d/limits.pbs_mom
+    sudo sed -i "s/^fi/#fi /g" /opt/pbs/lib/init.d/limits.pbs_mom
+    sudo systemctl enable pbs
+    sudo systemctl start pbs
 
-/opt/pbs/bin/qmgr -c "c n $(hostname)"
+    /opt/pbs/bin/qmgr -c "c n $(hostname)"
+else
+    echo "PBS client was already installed"
+fi
