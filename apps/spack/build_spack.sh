ed0698dd80387daf36fef57b14de0824a4133db6
Cormac Garvey <cogarvey@microsoft.com>
2019-12-03 12:26:17 -0700

    Added hpcx package and removed patch.

--- apps/spack/build_spack.sh
+++ apps/spack/build_spack.sh
@@ -261,35 +261,65 @@ compilers:
     target: x86_64
 EOF
 
-PATCH1=patch_web_py
-cat > ~/${PATCH1} << EOF
---- lib/spack/spack/util/web.py_orig    2019-09-20 00:04:49.140840143 +0000
-+++ lib/spack/spack/util/web.py 2019-09-20 00:05:33.820755495 +0000
-@@ -99,6 +99,7 @@
-        - pages: dict of pages visited (URL) mapped to their full text.
-        - links: set of links encountered while visiting the pages.
-     """
-+    url = url + '/index.html'
-     pages = {}     # dict from page URL -> text content.
-     links = set()  # set of all links seen on visited pages.
+HPCX_PACKAGE=package.py
+cat > ~/${HPCX_PACKAGE} << EOF
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-@@ -135,10 +136,10 @@
-             tty.debug("ignoring page " + url)
-             return pages, links
 
--        if not resp.headers["Content-type"].startswith('text/html'):
--            tty.debug("ignoring page " + url + " with content type " +
--                      resp.headers["Content-type"])
--            return pages, links
-+#        if not resp.headers["Content-type"].startswith('text/html'):
-+#            tty.debug("ignoring page " + url + " with content type " +
-+#                      resp.headers["Content-type"])
-+#            return pages, links
+import os
+import sys
+import llnl.util.tty as tty
 
-         # Do the real GET request when we know it's just HTML.
-         req.get_method = lambda: "GET"
+
+class Hpcx(AutotoolsPackage):
+    """An open source Message Passing Interface implementation.
+
+    The Open MPI Project is an open source Message Passing Interface
+    implementation that is developed and maintained by a consortium
+    of academic, research, and industry partners. Open MPI is
+    therefore able to combine the expertise, technologies, and
+    resources from all across the High Performance Computing
+    community in order to build the best MPI library available.
+    Open MPI offers advantages for system and software vendors,
+    application developers and computer science researchers.
+    """
+
+    homepage = "http://www.open-mpi.org"
+    url = "https://www.open-mpi.org/software/ompi/v4.0/downloads/openmpi-4.0.0.tar.bz2"
+    list_url = "http://www.open-mpi.org/software/ompi/"
+    git = "https://github.com/open-mpi/ompi.git"
+
+    version('develop', branch='master')
+
+    provides('mpi')
+
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        self.prefix.bin = "/opt/hpcx-v2.5.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-redhat7.7-x86_64/ompi/bin"
+        spack_env.set('MPICC',  join_path(self.prefix.bin, 'mpicc'))
+        spack_env.set('MPICXX', join_path(self.prefix.bin, 'mpic++'))
+        spack_env.set('MPIF77', join_path(self.prefix.bin, 'mpif77'))
+        spack_env.set('MPIF90', join_path(self.prefix.bin, 'mpif90'))
+
+        spack_env.set('OMPI_CC', spack_cc)
+        spack_env.set('OMPI_CXX', spack_cxx)
+        spack_env.set('OMPI_FC', spack_fc)
+        spack_env.set('OMPI_F77', spack_f77)
+
+    def setup_dependent_package(self, module, dependent_spec):
+        self.prefix.bin = "/opt/hpcx-v2.5.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-redhat7.7-x86_64/ompi/bin"
+        self.spec.mpicc = join_path(self.prefix.bin, 'mpicc')
+        self.spec.mpicxx = join_path(self.prefix.bin, 'mpic++')
+        self.spec.mpifc = join_path(self.prefix.bin, 'mpif90')
+        self.spec.mpif77 = join_path(self.prefix.bin, 'mpif77')
+        self.spec.mpicxx_shared_libs = [
+            join_path(self.prefix.lib, 'libmpi_cxx.{0}'.format(dso_suffix)),
+            join_path(self.prefix.lib, 'libmpi.{0}'.format(dso_suffix))
+        ]
 EOF
-
 
 sudo yum install -y python3
 
@@ -299,8 +329,8 @@ cd $SPACKDIR
 git clone https://github.com/spack/spack.git
 cd spack
 git checkout tags/v${APP_VERSION}
-mv ~/${PATCH1} .
-#patch -p0 < ${PATCH1}
+mkdir ${SPACKDIR}/spack/var/spack/repos/builtin/packages/hpcx
+cp $HPCX_PACKAGE  ${SPACKDIR}/spack/var/spack/repos/builtin/packages/hpcx
 source ${SPACKDIR}/spack/share/spack/setup-env.sh
 echo "source ${SPACKDIR}/spack/share/spack/setup-env.sh" >> ~/.bash_profile
 sudo mkdir /mnt/resource/spack
