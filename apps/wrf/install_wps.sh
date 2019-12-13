e255de7e96f8e229672426b59c3abf37d7b13140
Cormac Garvey <cogarvey@microsoft.com>
2019-12-13 15:41:28 -0700

    Extracted WPS tar file and changed to correct path before build WPS.

--- apps/wrf/install_wps.sh
+++ apps/wrf/install_wps.sh
@@ -6,6 +6,7 @@ WRF_VERSION=4.1.3
 
 cd $SHARED_APP
 wget https://github.com/wrf-model/WPS/archive/v${APP_VERSION}.tar.gz
+tar xvf v${APP_VERSION}.tar.gz
 
 spack load netcdf-fortran
 spack load netcdf
@@ -18,6 +19,7 @@ export HDF5=$(spack location -i hdf5)
 export NETCDF=$(spack location -i netcdf-fortran)
 export WRF_DIR=${SHARED_APP}/WRF-${WRF_VERSION}
 
+cd WPS-${APP_VERSION}
 ./configure << EOF
 3
 EOF
