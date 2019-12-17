ea0a5ec416b537989126c2a296b69511433342e8
Cormac Garvey <cogarvey@microsoft.com>
2019-12-17 13:27:14 -0700

    Changed name of mvapich2 install version of wps

--- apps/wrf/install_wps_mvapich2.sh
+++ apps/wrf/install_wps_mvapich2.sh
@@ -8,15 +8,15 @@ cd $SHARED_APP
 wget https://github.com/wrf-model/WPS/archive/v${APP_VERSION}.tar.gz
 tar xvf v${APP_VERSION}.tar.gz
 
-spack load netcdf-fortran
-spack load netcdf
-spack load hdf5
+spack load netcdf-fortran^mvapich2
+spack load netcdf^mvapich2
+spack load hdf5^mvapich2
 spack load perl
 spack load mvapich2
 module load gcc-9.2.0
 
-export HDF5=$(spack location -i hdf5)
-export NETCDF=$(spack location -i netcdf-fortran)
+export HDF5=$(spack location -i hdf5^mvapich2)
+export NETCDF=$(spack location -i netcdf-fortran^mvapich2)
 export WRF_DIR=${SHARED_APP}/WRF-${WRF_VERSION}
 
 cd WPS-${APP_VERSION}
