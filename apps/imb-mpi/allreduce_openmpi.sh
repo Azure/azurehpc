83431b463960425a3658eb9340adbc050d3b7b60
Xavier Pillons <xpillons@microsoft.com>
2020-01-20 17:18:43 +0100

    remove pkey

--- apps/imb-mpi/allreduce_openmpi.sh
+++ apps/imb-mpi/allreduce_openmpi.sh
@@ -3,15 +3,17 @@ source /etc/profile
 module use /usr/share/Modules/modulefiles
 module load mpi/hpcx
 
-PKEY=`cat /sys/class/infiniband/mlx5_0/ports/1/pkeys/* | grep -v 0000 | grep -v 0x7fff`
-PKEY=`echo "${PKEY/0x8/0x0}"`
-echo "PKEY=$PKEY"
+# PKEY is no longer needed on CentOS 7.7+
+#PKEY=`cat /sys/class/infiniband/mlx5_0/ports/1/pkeys/* | grep -v 0000 | grep -v 0x7fff`
+#PKEY=`echo "${PKEY/0x8/0x0}"`
+#echo "PKEY=$PKEY"
 
 mpi_options="--mca btl ^vader,tcp,openib,uct -mca pml ucx"
 mpi_options+=" --mca opal_warn_on_missing_libcuda 0"
 mpi_options+=" --map-by core"
 #mpi_options+=" --report-bindings --display-allocation"
-mpi_options+=" -x UCX_IB_PKEY=$PKEY -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=ud_x,sm,self"
+#mpi_options+=" -x UCX_IB_PKEY=$PKEY"
+mpi_options+=" -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=ud_x,sm,self"
 
 # Enable HCOLL
 mpi_options+=" --mca coll_hcoll_enable 1 -x coll_hcoll_np=0 -x HCOLL_MAIN_IB=mlx5_0:1"
