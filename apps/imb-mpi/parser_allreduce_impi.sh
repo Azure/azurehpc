6bc7fe9f46cdd42fbabe296d3d3c34d0d81b0526
Xavier Pillons <xpillons@microsoft.com>
2020-01-20 14:45:27 +0100

    fix output file name

--- apps/imb-mpi/parser_allreduce_impi.sh
+++ apps/imb-mpi/parser_allreduce_impi.sh
@@ -2,7 +2,7 @@
 source /etc/profile
 module load mpi/impi
 
-file=${1-output.log}
+file=output.log
 grep -A3 "t_min" ${file} | tr -s ' ' | \
     jq --slurp --raw-input --raw-output 'split("\n") | .[2:-1] | map(split(" ")) | map({"bytes": .[1]|tonumber, "iter": .[2]|tonumber, "t_min": .[3]|tonumber, "t_max": .[4]|tonumber, "t_avg": .[5]|tonumber})' > metrics.json
 
--- apps/imb-mpi/parser_allreduce_openmpi.sh
+++ apps/imb-mpi/parser_allreduce_openmpi.sh
@@ -3,7 +3,7 @@ source /etc/profile
 module use /usr/share/Modules/modulefiles
 module load mpi/hpcx
 
-file=${1-output.log}
+file=output.log
 grep -A3 "t_min" ${file} | tr -s ' ' | \
     jq --slurp --raw-input --raw-output 'split("\n") | .[2:-1] | map(split(" ")) | map({"bytes": .[1]|tonumber, "iter": .[2]|tonumber, "t_min": .[3]|tonumber, "t_max": .[4]|tonumber, "t_avg": .[5]|tonumber})' > metrics.json
 
