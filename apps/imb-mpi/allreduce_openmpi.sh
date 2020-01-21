442ef12f0b5d4abe975aa97bdcae3048afba4160
Xavier Pillons <xpillons@microsoft.com>
2020-01-21 10:04:25 +0100

    add PKEY again

--- apps/imb-mpi/allreduce_openmpi.sh
+++ apps/imb-mpi/allreduce_openmpi.sh
@@ -4,15 +4,16 @@ module use /usr/share/Modules/modulefiles
 module load mpi/hpcx
 
 # PKEY is no longer needed on CentOS 7.7+
-#PKEY=`cat /sys/class/infiniband/mlx5_0/ports/1/pkeys/* | grep -v 0000 | grep -v 0x7fff`
-#PKEY=`echo "${PKEY/0x8/0x0}"`
-#echo "PKEY=$PKEY"
+# TODO : use cat /etc/centos-release to retrieve version and set PKEY if lower than 7.6
+PKEY=`cat /sys/class/infiniband/mlx5_0/ports/1/pkeys/* | grep -v 0000 | grep -v 0x7fff`
+PKEY=`echo "${PKEY/0x8/0x0}"`
+echo "PKEY=$PKEY"
 
 mpi_options="--mca btl ^vader,tcp,openib,uct -mca pml ucx"
 mpi_options+=" --mca opal_warn_on_missing_libcuda 0"
 mpi_options+=" --map-by core"
 #mpi_options+=" --report-bindings --display-allocation"
-#mpi_options+=" -x UCX_IB_PKEY=$PKEY"
+mpi_options+=" -x UCX_IB_PKEY=$PKEY"
 mpi_options+=" -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=ud_x,sm,self"
 
 # Enable HCOLL
