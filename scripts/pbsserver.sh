344026c3dea956116d5a51932fe72448d7c8a946
JonShelley <jshelley.menan@gmail.com>
2020-01-24 16:23:00 +0000

    added pool_name as a resource for job scheduling

--- scripts/pbsserver.sh
+++ scripts/pbsserver.sh
@@ -9,6 +9,10 @@ if [ "$(rpm -qa pbspro-server)" = "" ];then
     sudo /opt/pbs/bin/qmgr -c "s s managers += ${admin_user}@*"
     sudo /opt/pbs/bin/qmgr -c 's s flatuid=t'
     sudo /opt/pbs/bin/qmgr -c 's s job_history_enable=t'
+    sudo /opt/pbs/bin/qmgr -c 'c r pool_name type=string,flag=h'
+
+    # Update the sched_config file to schedule jobs that request pool_name
+    sudo sed -i "s/^resources: \"ncpus,/resources: \"ncpus, pool_name,/g" /var/spool/pbs/sched_priv/sched_config
 else
     echo "PBSPro already installed"
 fi
