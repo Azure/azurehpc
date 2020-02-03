057c5c1e4e2f4a03bf1efc16acc23418dc9b0f3c
Xavier Pillons <xpillons@microsoft.com>
2020-02-03 18:52:55 +0100

    Update install-azcopy.sh

--- scripts/install-azcopy.sh
+++ scripts/install-azcopy.sh
@@ -2,4 +2,4 @@
 
 cd /usr/local/bin
 wget -q https://aka.ms/downloadazcopy-v10-linux -O - | tar zxf - --strip-components 1 --wildcards '*/azcopy'
-chmod +x /usr/local/bin/azcopy 
+chmod 755 /usr/local/bin/azcopy 
