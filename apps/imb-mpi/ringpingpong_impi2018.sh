8429d8fe66143442d971a37ea985f614478cb06f
Xavier Pillons <xpillons@microsoft.com>
2020-02-05 16:46:17 +0100

    pingpong for impi2018 and impi2019

--- apps/imb-mpi/ringpingpong_impi2018.sh
+++ apps/imb-mpi/ringpingpong_impi2018.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
+set -o pipefail
 # setup Intel MPI environment for Infiniband
-source /etc/profile # so we can load modules
-module load mpi/impi-2019
-source $MPI_BIN/mpivars.sh -ofi_internal
+source /etc/profile
+module load mpi/impi
+source $MPI_BIN/mpivars.sh
 
 hostlist=$(pwd)/hosts.$PBS_JOBID
 
 sort -u $PBS_NODEFILE > $hostlist
 
-export I_MPI_FABRICS="shm:ofi"
+export I_MPI_FABRICS="shm:ofa"
 export I_MPI_FALLBACK_DEVICE=0
 export I_MPI_DEBUG=4
 
 src=$(tail -n 1 $hostlist)
+# -msglog 10:10 is for 1024 bytes message size only
 for dst in $(<$hostlist); do
-    mpirun -np 2 -ppn 1 -hosts $src,$dst IMB-MPI1 PingPong > ${src}_to_${dst}_ringpingpong.$PBS_JOBID.log 2>&1
+    mpirun -np 2 -ppn 1 -hosts $src,$dst IMB-MPI1 PingPong -msglog 10:10 > ${src}_to_${dst}_ringpingpong.$PBS_JOBID.log 2>&1
     src=$dst
 done
 
