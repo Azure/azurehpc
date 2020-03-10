e3387279b77bd51fc3c58e8f86a2d6f54e1e6130
Xavier Pillons <xpillons@microsoft.com>
2020-03-10 17:21:51 +0100

    added nbclients

--- apps/fio/parser_fio.sh
+++ apps/fio/parser_fio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILESYSTEM=$1
 
-grep 'READ\|WRITE' *.out | sed 's/(//g;s/)//g;s/MB/ MB/g;s/:/ :/g;s/_/ /g;s/.out//g' | tr -s ' ' | jq --slurp --raw-input --raw-output 'split("\n")  | .[:-1] | map(split(" ")) |  map({"io_type": .[2], "file_size": .[3], "block_size": .[4], "bw": .[9]|tonumber, "unit": .[10]})' > metrics.json
+grep 'READ\|WRITE' *.out | sed 's/(//g;s/)//g;s/MB/ MB/g;s/:/ :/g;s/_/ /g;s/.out//g' | tr -s ' ' | jq --slurp --raw-input --raw-output 'split("\n")  | .[:-1] | map(split(" ")) |  map({"io_type": .[2], "file_size": .[3], "block_size": .[4], "nbclients": .[5]|tonumber, "bw": .[10]|tonumber, "unit": .[11]})' > metrics.json
 
 cat <<EOF >app.json
 {
