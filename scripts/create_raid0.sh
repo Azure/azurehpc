099976d4eb02a71ca66a7b21ae5fdd764c889a17
edwardsp <paul.m.edwards@gmail.com>
2020-03-17 15:13:15 +0000

    Fixing incorrect partition name
    If a device ends in a number it seems to add "p1" otherwise just "1" for the first partition.  Reverting back to the previous way which lets fdisk tell us.

--- scripts/create_raid0.sh
+++ scripts/create_raid0.sh
@@ -20,7 +20,7 @@ for disk in $devices; do
     parted -s $disk print
     parted -s $disk "set 1 raid on"
 
-    partitions="$partitions ${disk}p1"
+    partitions="$partitions $(fdisk -l $disk | grep '^/dev' | cut -d' ' -f1)"
 done
 
 ndevices=$(echo $partitions | wc -w)
