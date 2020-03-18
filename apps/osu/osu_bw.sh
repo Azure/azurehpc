93ab2ce20d189c5ae6183ab734fddd200de5f20e
Xavier Pillons <xpillons@microsoft.com>
2020-03-18 18:28:16 +0100

    simplify hpcx options

--- apps/osu/osu_bw.sh
+++ apps/osu/osu_bw.sh
@@ -6,20 +6,6 @@ source /etc/profile
 module use /usr/share/Modules/modulefiles
 module load mpi/hpcx
 
-PKEY=`cat /sys/class/infiniband/mlx5_0/ports/1/pkeys/* | grep -v 0000 | grep -v 0x7fff`
-PKEY=`echo "${PKEY/0x8/0x0}"`
-echo "PKEY=$PKEY"
-
-mpi_options+=" --mca btl self"
-
-# Use UCX
-mpi_options+=" --mca pml ucx --mca osc ucx"
-mpi_options+=" -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_IB_PKEY=$PKEY -x UCX_LOG_LEVEL=error"
-# Enable HCOLL
-mpi_options+=" --mca coll_hcoll_enable 1 -x coll_hcoll_np=0 -x HCOLL_MAIN_IB=mlx5_0:1"
-# Tune collectives
-mpi_options+=" -x HCOLL_ENABLE_MCAST_ALL=1 -x HCOLL_MCAST_NP=0 -x HCOLL_CONTEXT_CACHE_ENABLE=1"
-
 mpi_options+=" -x LD_LIBRARY_PATH"
 mpi_options+=" -bind-to core"
 mpi_options+=" -npernode 1 -np 2"
