d3ebd694a2cbcb0685f4eebf5c662bef1092fa3c
Xavier Pillons <xpillons@microsoft.com>
2020-03-19 18:24:04 +0100

    fix pypath

--- libexec/pyazhpc-preprocess.sh
+++ libexec/pyazhpc-preprocess.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export azhpc_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-PYTHONPATH=$PYTHONPATH:$AZHPC_PYTHONPATH
-$azhpc_pypath $azhpc_dir/pyazhpc/azhpc.py preprocess $*
+PYTHONPATH=$PYTHONPATH:$AZHPC_PYTHONPATH $azhpc_pypath $azhpc_dir/pyazhpc/azhpc.py preprocess $*
 
