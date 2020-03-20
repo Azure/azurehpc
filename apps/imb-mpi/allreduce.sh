04da8ccacaeaf00c7a25bd5dad3a67cc5634c892
Xavier Pillons <xpillons@microsoft.com>
2020-03-20 16:13:35 +0100

    fix breaking change of impi modules

--- apps/imb-mpi/allreduce.sh
+++ apps/imb-mpi/allreduce.sh
@@ -9,21 +9,30 @@ num_ranks=$(wc -l <$PBS_NODEFILE)
 case $MPI in
     impi2018)
         module load mpi/impi
-        source $MPI_BIN/mpivars.sh
+        #source $MPI_BIN/mpivars.sh
         export I_MPI_FABRICS="shm:ofa"
         export I_MPI_FALLBACK_DEVICE=0
         export I_MPI_DEBUG=4
         mpi_options="-hostfile $PBS_NODEFILE -np $num_ranks"
-        IMB_ROOT=$MPI_BIN
+        if [ -z $MPI_BIN ]; then
+            IMB_ROOT=$I_MPI_ROOT/intel64/bin
+        else
+            IMB_ROOT=$MPI_BIN
+        fi
     ;;
     impi2019)
         module load mpi/impi-2019
-        source $MPI_BIN/mpivars.sh -ofi_internal
+        #source $MPI_BIN/mpivars.sh -ofi_internal
         export I_MPI_FABRICS="shm:ofi"
         export I_MPI_FALLBACK_DEVICE=0
         export I_MPI_DEBUG=4
+        export FI_PROVIDER=mlx
         mpi_options="-hostfile $PBS_NODEFILE -np $num_ranks"
-        IMB_ROOT=$MPI_BIN
+        if [ -z $MPI_BIN ]; then
+            IMB_ROOT=$I_MPI_ROOT/intel64/bin
+        else
+            IMB_ROOT=$MPI_BIN
+        fi
     ;;
     ompi)
         module load mpi/hpcx
