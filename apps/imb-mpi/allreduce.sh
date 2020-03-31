416bf87dc1b679621f45c659bf9e89ade62328ca
Xavier Pillons <xpillons@microsoft.com>
2020-03-31 11:21:10 +0200

    remove fallback as it's no longer supported

--- apps/imb-mpi/allreduce.sh
+++ apps/imb-mpi/allreduce.sh
@@ -24,7 +24,7 @@ case $MPI in
         module load mpi/impi-2019
         #source $MPI_BIN/mpivars.sh -ofi_internal
         export I_MPI_FABRICS="shm:ofi"
-        export I_MPI_FALLBACK_DEVICE=0
+        #export I_MPI_FALLBACK_DEVICE=0
         export I_MPI_DEBUG=4
         export FI_PROVIDER=verbs
         mpi_options="-hostfile $PBS_NODEFILE -np $num_ranks"
