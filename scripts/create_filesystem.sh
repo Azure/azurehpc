7a4d6ef7ef5397289fadbe924157b045b12cfba0
Xavier Pillons <xpillons@microsoft.com>
2020-04-02 14:18:03 +0200

    exit on error

--- scripts/create_filesystem.sh
+++ scripts/create_filesystem.sh
@@ -11,14 +11,14 @@ fi
 echo "Creating a $filesystem filesystem on device $device mounted on $mount"
 
 if [ "$filesystem" == "xfs" ]; then
-    mkfs -t $filesystem /dev/$device
-    xfsuuid="UUID=`blkid |grep dev/$device |cut -d " " -f 2 |cut -c 7-42`"
+    mkfs -t $filesystem $device || exit 1
+    xfsuuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
     echo "$xfsuuid $mount $filesystem rw,noatime,attr2,inode64,nobarrier,nofail 0 2" >> /etc/fstab
 else
-    mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype /dev/$device
+    mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype $device || exit 1
     sleep 5
-    tune2fs -o user_xattr /dev/$device
-    ext4uuid="UUID=`blkid |grep dev/$device |cut -d " " -f 2 |cut -c 7-42`"
+    tune2fs -o user_xattr $device
+    ext4uuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
     echo "$ext4uuid $mount $filesystem noatime,nodiratime,nobarrier,nofail 0 2" >> /etc/fstab
 fi
 
