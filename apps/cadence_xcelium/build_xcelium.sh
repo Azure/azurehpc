44228223402ab2b0f8c367922bc6f3886d6bc2b7
mengruts <mengruts@gmail.com>
2020-04-08 21:07:28 +0800

    Update build_xcelium.sh

--- apps/cadence_xcelium/build_xcelium.sh
+++ apps/cadence_xcelium/build_xcelium.sh
@@ -36,24 +36,23 @@ install_iscape()
 download_xcelium()
 {
         echo "----------------------downloading Xcelium."
-        sudo wget -P /mnt/resource/xcelium https://edatools.blob.core.windows.net/cadence/Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/Base_XCELIUMMAIN20.03.001_lnx86.sdx
-        sudo wget -P /mnt/resource/xcelium https://edatools.blob.core.windows.net/cadence/Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/ic_index.sdx
+        sudo wget -P /mnt/resource/xcelium ${CADENCE_TOOLS_BLOB}Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/Base_XCELIUMMAIN20.03.001_lnx86.sdx
+        sudo wget -P /mnt/resource/xcelium ${CADENCE_TOOLS_BLOB}Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/ic_index.sdx
 }
 
 install_xcelium()
 {
         echo "---------------------installing Xcelium."
-        # install from archive
-        sudo ./iscape.sh -batch majorAction=installfromarchive ArchiveDirectory=/mnt/resource/xcelium/  InstallDirectory=/datadrive/cadence/
+        sudo ./iscape.sh -batch majorAction=installfromarchive ArchiveDirectory=${WORKING_DIR}/xcelium/  InstallDirectory=${INSTALL_DIR}/cadence/
 
         # generate configuration scripts
-        sudo ./iscape.sh -batch majorAction=configure InstallDirectory=/datadrive/cadence/
+        sudo ./iscape.sh -batch majorAction=configure InstallDirectory=${INSTALL_DIR}/cadence/
 
         # complete configuration
-        sudo /bin/sh /datadrive/cadence/installData/XCELIUM2003_lnx86/batch_configure.sh
+        sudo /bin/sh ${INSTALL_DIR}/cadence/installData/XCELIUM2003_lnx86/batch_configure.sh
 
         # test
-        sudo ./iscape.sh -batch majorAction=test InstallDirectory=/datadrive/cadence/
+        sudo ./iscape.sh -batch majorAction=test InstallDirectory=${INSTALL_DIR}/cadence/
 }
 
 install_required_packages
