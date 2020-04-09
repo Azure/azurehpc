539844c08cf82e1bd632c65a644e93248432e495
Xavier Pillons <xpillons@microsoft.com>
2020-04-09 16:25:22 +0200

    added

---
+++ scripts/install-azcli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sh -c 'echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+
+yum install -y azure-cli
+
