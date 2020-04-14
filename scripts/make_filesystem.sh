8c5f0015d38e9c4e9a0616d8fcd22c887080e088
Xavier Pillons <xpillons@microsoft.com>
2020-04-14 12:09:48 +0200

    added ext4 and new tests

--- scripts/make_filesystem.sh
+++ scripts/make_filesystem.sh
@@ -10,17 +10,26 @@ fi
 
 echo "Creating a $filesystem filesystem on device $device mounted on $mount"
 
-if [ "$filesystem" == "xfs" ]; then
-    mkfs -t $filesystem $device || exit 1
-    xfsuuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
-    echo "$xfsuuid $mount $filesystem rw,noatime,attr2,inode64,nobarrier,nofail 0 2" >> /etc/fstab
-else
-    mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype $device || exit 1
-    sleep 5
-    tune2fs -o user_xattr $device
-    ext4uuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
-    echo "$ext4uuid $mount $filesystem noatime,nodiratime,nobarrier,nofail 0 2" >> /etc/fstab
-fi
+case $filesystem in
+    xfs)
+        mkfs -t $filesystem $device || exit 1
+        xfsuuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
+        echo "$xfsuuid $mount $filesystem rw,noatime,attr2,inode64,nobarrier,nofail 0 2" >> /etc/fstab
+    ;;
+
+    ext4)
+        mkfs.ext4 -i 2048 -I 512 -J size=400 -Odir_index,filetype $device || exit 1
+        sleep 5
+        tune2fs -o user_xattr $device
+        ext4uuid="UUID=`blkid |grep $device |cut -d " " -f 2 |cut -c 7-42`"
+        echo "$ext4uuid $mount $filesystem noatime,nodiratime,nobarrier,nofail 0 2" >> /etc/fstab
+    ;;
+
+    *)
+        echo "filesystem not supported"
+        exit 1
+    ;;
+esac
 
 sleep 10
 mkdir $mount
