95bbf1c28a00467697409605ca64efe7578ac628
dazzag24 <dazzag@gmail.com>
2020-04-22 14:36:32 +0100

    Fix install_wps_openmpi.sh
    1. Allow the WRF version to be overridden:
    WRF_VERSION=${WRF_VERSION:-4.1.3}
    
    2. Comment out:
    spack load netcdf^openmpi
    
    as it causes this error/warning:
    ==> Error: Spec 'netcdf ^openmpi' matches no installed packages.
    
    3. Fix error in export line, should be "wrf-openmpi" not "wrf-openmp"

--- apps/wrf/install_wps_openmpi.sh
+++ apps/wrf/install_wps_openmpi.sh
@@ -4,8 +4,9 @@ APP_NAME=wps
 APP_VERSION=4.1
 SKU_TYPE=${SKU_TYPE:-hb}
 SHARED_APP=${SHARED_APP:-/apps}
-WRF_VERSION=4.1.3
+WRF_VERSION=${WRF_VERSION:-4.1.3}
 APP_DIR=${SHARED_APP}/${SKU_TYPE}/${APP_NAME}-openmpi
+OPENMPI_VER=4.0.3
 
 sudo yum install -y jasper-devel
 sudo yum install -y libpng-devel
@@ -18,15 +19,15 @@ tar xvf v${APP_VERSION}.tar.gz
 source ${SPACK_ROOT}/share/spack/setup-env.sh
 
 spack load netcdf-fortran^openmpi
-spack load netcdf^openmpi
+#spack load netcdf^openmpi
 spack load hdf5^openmpi
 spack load perl
-module load mpi/openmpi-4.0.2
+module load mpi/openmpi-${OPENMPI_VER}
 module load gcc-9.2.0
 
 export HDF5=$(spack location -i hdf5^openmpi)
 export NETCDF=$(spack location -i netcdf-fortran^openmpi)
-export WRF_DIR=${SHARED_APP}/${SKU_TYPE}/wrf-openmp/WRF-${WRF_VERSION}
+export WRF_DIR=${SHARED_APP}/${SKU_TYPE}/wrf-openmpi/WRF-${WRF_VERSION}
 
 cd WPS-${APP_VERSION}
 ./configure << EOF
