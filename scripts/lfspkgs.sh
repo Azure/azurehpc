80a07ee1eddae356dae2c254a6f0d6817ae8401e
Xavier Pillons <xpillons@microsoft.com>
2020-04-23 17:23:23 +0200

    make yum failed in case of error

--- scripts/lfspkgs.sh
+++ scripts/lfspkgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y install lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests
+yum -y install lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests || exit 1
 
 sed -i 's/ResourceDisk\.Format=y/ResourceDisk.Format=n/g' /etc/waagent.conf
 
