72c5ddc60453f009ead3d81d609eae1bb300b6c4
Paul Edwards <Paul.Edwards@Microsoft.com>
2020-04-27 17:22:57 +0100

    Fixed launch of azhpc

--- examples/slurm_example/scripts/resume.sh
+++ examples/slurm_example/scripts/resume.sh
@@ -11,7 +11,8 @@ az login --identity -o table
 source /apps/slurm/azurehpc/install.sh
 
 cd /apps/slurm/azscale
-python3 $azhpc_dir/pyazhpc/azhpc.py slurm_resume "$NODES" >> /var/log/slurm/autoscale.log 2>&1
+export PYTHONPATH=$PYTHONPATH:$AZHPC_PYTHONPATH
+$azhpc_pypath $azhpc_dir/pyazhpc/azhpc.py slurm_resume "$NODES" >> /var/log/slurm/autoscale.log 2>&1
 
 resource_group=$(azhpc-get resource_group | tr ' ' '\n' | tail -n 1)
 
--- examples/slurm_example/scripts/suspend.sh
+++ examples/slurm_example/scripts/suspend.sh
@@ -10,6 +10,7 @@ az login --identity
 source /apps/slurm/azurehpc/install.sh
 
 cd /apps/slurm/azscale
-python3 $azhpc_dir/pyazhpc/azhpc.py slurm_suspend "$NODES" >> /var/log/slurm/autoscale.log 2>&1
+export PYTHONPATH=$PYTHONPATH:$AZHPC_PYTHONPATH
+$azhpc_pypath $azhpc_dir/pyazhpc/azhpc.py slurm_suspend "$NODES" >> /var/log/slurm/autoscale.log 2>&1
 
 echo "$(date) : [suspend.sh] : exiting" >> /var/log/slurm/autoscale.log
