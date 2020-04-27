e1fea95f209fdb3263498c592d2ef2986e599f15
Cormac Garvey <cogarvey@microsoft.com>
2020-04-27 17:58:11 -0500

    Started glusterfs volume

--- scripts/glusterfs_vol.sh
+++ scripts/glusterfs_vol.sh
@@ -3,6 +3,8 @@
 HOSTLIST=$1
 REPLICA=$2
 
+GLUSTERFS_VOL_NAME=glusterfs_vol
+
 if [ "$PSSH_NODENUM" = "0" ]; then
    for host in `cat hostlists/$HOSTLIST`
    do
@@ -15,7 +17,9 @@ if [ "$PSSH_NODENUM" = "0" ]; then
       replica_str="replica $REPLICA"
    fi
 
-   gluster volume create flusterfs_vol $replica_str $hosts_str
+   gluster volume create $GLUSTERFS_VOL_NAME $replica_str $hosts_str
+
+   gluster volume start $GLUSTERFS_VOL_NAME
 
    gluster volume info
 fi
