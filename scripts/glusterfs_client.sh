f3c77bcf85b765a35cf80c3636c2c6a772d91e6a
Cormac Garvey <cogarvey@microsoft.com>
2020-04-27 19:50:11 -0500

    Corrected fstab syntax

--- scripts/glusterfs_client.sh
+++ scripts/glusterfs_client.sh
@@ -3,6 +3,6 @@
 NODE=$1
 MOUNT_PT=$2
 
-echo "mount -t glusterfs ${NODE}:/glusterfs_vol $MOUNT_PT flock" >> /etc/fstab
+echo "${NODE}:/glusterfs_vol $MOUNT_PT glusterfs defaults,_netdev 0 0" >> /etc/fstab
 mount -a
 chmod 777 $MOUNT_PT
