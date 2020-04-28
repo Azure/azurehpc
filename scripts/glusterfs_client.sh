b669642a23a6f39590bf70f63b652bddb894a825
Cormac Garvey <cogarvey@microsoft.com>
2020-04-27 20:06:36 -0500

    Corrected name of glusterfs vol

--- scripts/glusterfs_client.sh
+++ scripts/glusterfs_client.sh
@@ -3,6 +3,6 @@
 NODE=$1
 MOUNT_PT=$2
 
-echo "${NODE}:/glusterfs_vol $MOUNT_PT glusterfs defaults,_netdev 0 0" >> /etc/fstab
+echo "${NODE}:/gv0 $MOUNT_PT glusterfs defaults,_netdev 0 0" >> /etc/fstab
 mount -a
 chmod 777 $MOUNT_PT
