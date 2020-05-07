c2c3b9b4b7cfac7afcffa75d6b1b551bfe1cdf85
Xavier Pillons <xpillons@microsoft.com>
2020-05-07 11:42:06 +0200

    fix logical test

--- ci/build.sh
+++ ci/build.sh
@@ -68,7 +68,7 @@ azhpc-build -c $config_file $AZHPC_OPTION
 return_code=$?
 cat deploy*.json
 ls -al
-if [[ "$return_code" -ne "0" ] || [ "$show_logs" == "true" ]]; then
+if [[ "$return_code" -ne "0" ]] || [[ "$show_logs" == "true" ]]; then
     config_file_no_path=${config_file##*/}
     config_file_no_path_or_extension=${config_file_no_path%.*}
     tmp_dir=azhpc_install_$config_file_no_path_or_extension
