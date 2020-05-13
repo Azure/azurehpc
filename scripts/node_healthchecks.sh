6d6df9879bb19325173cbf123d04b28bc4f3f0d3
Xavier Pillons <xpillons@microsoft.com>
2020-05-13 17:32:43 +0200

    first health checks

--- scripts/node_healthchecks.sh
+++ scripts/node_healthchecks.sh
@@ -5,35 +5,34 @@
 # - IB speed
 # - GPU device presence
 # - memory stream performance
+# 
+# Dependencies : jq, azcopy, healthchecks.json
+#
+# If a node is unhealty its metadata will be stored into the container for which the full_sas_key is provided. The storage structure will be YYYY/MM/DD/HH/location
+full_sas_key=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+bad_node=0
 
-mds_api_version="2018-10-01"
+# Retrieve the VM size
+AZHPC_VMSIZE=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize')
 
-# Retrieve the VM size and convert it to lowercase
-AZHPC_VMSIZE=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=$mds_api_version" | jq '.vmSize')
-AZHPC_VMSIZE=${AZHPC_VMSIZE,,}
-
-
-function check_ib()
+function check_ib_device()
 {
     case $AZHPC_VMSIZE in
-        standard_h16mr|standard_h16r)
+        Standard_H16mr|Standard_H16r)
             ib_device=$(ifconfig | grep eth1 -A1 | grep inet | tr -s ' ' | cut -d' ' -f 3)
             if [ -n "$ib_device" ]; then
                 IB_STATE=$(cat /sys/class/infiniband/*/ports/1/state | awk -F ":" '{print $2}' | xargs)  2>/dev/null
                 IB_PHYS_STATE=$(cat /sys/class/infiniband/*/ports/1/phys_state | awk -F ":" '{print $2}'| xargs)  2>/dev/null
                 IB_RATE=$(cat /sys/class/infiniband/*/ports/1/rate)  2>/dev/null
-                IB_SPEED=$(/sbin/ethtool eth1 | grep "Speed:" | awk '{print $2}'| cut -d M -f1 | xargs)  2>/dev/null
-                if [ "$IB_STATE" != "ACTIVE" ]; then
-                    echo "ERROR : Bad IB state : $IB_STATE"
-                    bad_node=1
-                fi
+                IB_SPEED=$(/sbin/ethtool eth1 | grep "Speed:" | awk '{print $2}'| xargs)  2>/dev/null
             else
                 echo "ERROR : No IB devices found"
                 bad_node=1
             fi
         ;;
 
-        standard_hc44rs|standard_hb60rs|standard_hb120rs_v2)
+        Standard_HC44rs|Standard_HB60rs|Standard_HB120rs_v2)
             # Retrieve IB info
             ib_device=$(ifconfig | grep ib0 -A1 | grep inet | tr -s ' ' | cut -d' ' -f 3)
             if [ -n "$ib_device" ]; then
@@ -41,15 +40,79 @@ function check_ib()
                 IB_RATE=$(ibv_devinfo -v | grep active_width | cut -d':' -f2 | xargs | cut -d' ' -f1)
                 IB_SPEED=$(ibv_devinfo -v | grep active_speed | cut -d':' -f2 | xargs | cut -d'(' -f1 | xargs)
                 IB_PHYS_STATE=$(ibv_devinfo -v | grep phys_state | cut -d':' -f2 | xargs | cut -d' ' -f1)
-                if [ "$IB_STATE" != "PORT_ACTIVE" ]; then
-                    echo "ERROR : Bad IB state : $IB_STATE"
-                    bad_node=1
-                fi
             else
                 echo "ERROR : No IB devices found"
                 bad_node=1
             fi
         ;;
+
+        *)
+            echo "uncovered VM Size $AZHPC_VMSIZE"
+            exit 1
+        ;;
     esac
 
+    check_ib_values $AZHPC_VMSIZE "$IB_STATE" "$IB_RATE" "$IB_SPEED" "$IB_PHYS_STATE"
 }
+
+function check_ib_values()
+{
+    vmsize=$1
+    state=$2
+    rate=$3
+    speed=$4
+    phys_state=$5
+
+    # Read the expected values from the dictionary config file
+    dictionary=$(jq '.infiniband[] | select(.sku==$vmsize)' --arg vmsize $vmsize $DIR/healthchecks.json)
+    expected=$(echo $dictionary | jq -r '.state')
+    if [ "$state" != "$expected" ]; then
+        echo "ERROR : IB state is $state while expected is $expected"
+        bad_node=1
+    fi
+    expected=$(echo $dictionary | jq -r '.rate')
+    if [ "$rate" != "$expected" ]; then
+        echo "ERROR : IB rate is $rate while expected is $expected"
+        bad_node=1
+    fi
+    expected=$(echo $dictionary | jq -r '.speed')
+    if [ "$speed" != "$expected" ]; then
+        echo "ERROR : IB speed is $speed while expected is $expected"
+        bad_node=1
+    fi
+    expected=$(echo $dictionary | jq -r '.phys_state')
+    if [ "$phys_state" != "$expected" ]; then
+        echo "ERROR : IB physical state is $phys_state while expected is $expected"
+        bad_node=1
+    fi
+
+}
+
+check_ib_device
+
+if [ $bad_node -eq 0 ]; then
+    echo "VM is healthy"
+else
+    echo "VM is unhealthy"
+
+    # If the sas key is not empty, get the node metadata and upload it into a blob
+    if [ "$full_sas_key" != "" ]; then
+        # Get simple metadata
+        hostname=$(hostname)
+        curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2019-08-15" > $hostname.json
+        d=$(date +"%Y/%m/%d/%H")
+        location=$(jq -r '.compute.location' $hostname.json)
+        blob="$d/$location/$AZHPC_VMSIZE_$hostname.json"
+        sas=$(echo ${full_sas_key#*\?})
+        uri=$(echo ${full_sas_key%\?*})
+
+        # if azcopy is missing install it
+        if [ ! -e /usr/local/bin/azcopy ]; then
+            echo "installing azcopy"
+            $DIR/install-azcopy.sh
+        fi
+        /usr/local/bin/azcopy cp $hostname.json "$uri/$blob?$sas"
+    fi
+fi
+
+exit $bad_node
