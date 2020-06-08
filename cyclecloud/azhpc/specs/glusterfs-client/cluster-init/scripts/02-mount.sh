2ed604eda3050bca08ea78557a09578442435699
Cormac Garvey <cogarvey@microsoft.com>
2020-06-08 17:22:56 -0500

    Access GlusterFS spec args here and not from jetpack

--- cyclecloud/azhpc/specs/glusterfs-client/cluster-init/scripts/02-mount.sh
+++ cyclecloud/azhpc/specs/glusterfs-client/cluster-init/scripts/02-mount.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-glusterfs_mounthost=$(jetpack config glusterfs.mount_host)
-glusterfs_mount_pt=$(jetpack config glusterfs
+#glusterfs_mounthost=$(jetpack config glusterfs.mount_host)
+#glusterfs_mount_pt=$(jetpack config glusterfs.mount_point)
+
+glusterfs_mounthost=${1:-glusterfs000000}
+glusterfs_mount_pt=${2:-/glusterfs}
 
 . $CYCLECLOUD_PROJECT_PATH/$CYCLECLOUD_SPEC_NAME/files/glusterfs_client.sh $glusterfs_mounthost $glusterfs_mount_pt
