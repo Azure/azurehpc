1bc38d5b7ce5418979fe70e8ba4f39bdd19cdabc
mengruts <mengruts@gmail.com>
2020-06-09 13:49:34 +0800

    add arg $ANFMountPoint from template

--- cyclecloud/azhpc/specs/anf-client/cluster-init/scripts/02-mount.sh
+++ cyclecloud/azhpc/specs/anf-client/cluster-init/scripts/02-mount.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. $CYCLECLOUD_PROJECT_PATH/$CYCLECLOUD_SPEC_NAME/files/replace_nfs_with_anf.sh /netapps
+. $CYCLECLOUD_PROJECT_PATH/$CYCLECLOUD_SPEC_NAME/files/replace_nfs_with_anf.sh $ANFMountPoint
