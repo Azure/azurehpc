5f97ef458f312251634c6e1772e94b79bffd8c3f
mengruts <mengruts@gmail.com>
2020-06-10 14:24:18 +0800

    anfmountpath & anfmountpoint as args

--- cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
+++ cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
@@ -2,7 +2,10 @@
 yum install -y nfs-utils
 
 mkdir -p /netapps
-echo "10.2.8.4:/raymondanfvol /netapps nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
+
+anfmountpath = $1
+anfmountpoint = $2
+echo "$anfmountpath} $anfmountpoint /netapps nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
 
 mount -a
 
