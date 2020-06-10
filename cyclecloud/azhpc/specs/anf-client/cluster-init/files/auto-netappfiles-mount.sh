2ae5e2b0c054bc2a14e8b3cdd88717404a89da3f
mengruts <mengruts@gmail.com>
2020-06-10 14:44:35 +0800

    Update auto-netappfiles-mount.sh

--- cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
+++ cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
@@ -5,7 +5,7 @@ mkdir -p /netapps
 
 anfmountpath = $1
 anfmountpoint = $2
-echo "$anfmountpath $anfmountpoint /netapps nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
+echo "$anfmountpath $anfmountpoint nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
 
 mount -a
 
