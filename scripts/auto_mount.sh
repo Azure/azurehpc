623230a444495d4ae3731188fd67f11f703f9908
Xavier Pillons <xpillons@microsoft.com>
2020-06-11 10:51:58 +0200

    mount only the mountpoint

--- scripts/auto_mount.sh
+++ scripts/auto_mount.sh
@@ -8,6 +8,6 @@ mkdir -p $mountpoint
 
 echo "$mountpath $mountpoint nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
 
-mount -a
+mount $mountpoint
 
 chmod 777 $mountpoint
