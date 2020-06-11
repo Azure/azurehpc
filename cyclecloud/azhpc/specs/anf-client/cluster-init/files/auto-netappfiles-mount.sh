853c9e454d78968d85060b2b71c7e9cf414a1a7e
mengruts <mengruts@gmail.com>
2020-06-11 08:00:04 +0800

    add $filesystem for generic use

--- cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
+++ cyclecloud/azhpc/specs/anf-client/cluster-init/files/auto-netappfiles-mount.sh
@@ -3,11 +3,12 @@ yum install -y nfs-utils
 
 anfmountpath = $1
 anfmountpoint = $2
+filesystem = "nfs"
 
 mkdir -p $anfmountpoint
 
 
-echo "$anfmountpath $anfmountpoint nfs bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
+echo "$anfmountpath $anfmountpoint $filesystem bg,rw,hard,noatime,nolock,rsize=65536,wsize=65536,vers=3,tcp,_netdev 0 0" >>/etc/fstab
 
 mount -a
 
