0096a720bab13b91caa7f3d386e033460c8c3950
Xavier Pillons <xpillons@microsoft.com>
2020-06-15 18:48:40 +0200

    new init

--- examples/cc_beegfs/init.sh
+++ examples/cc_beegfs/init.sh
@@ -5,21 +5,44 @@ locker="locker$uuid_str"
 jq '.projectstore=$locker' --arg locker $locker variables.json > temp.json
 cp temp.json variables.json
 
+cat <<EOF >config.json
+{}
+EOF
+
 function init_config()
 {
     local config=$1
     azhpc-init -d . -c $config
     config_file=${config##*/}
-    cp $config_file temp.json
-    jq '.variables+=$variables' --argjson variables "$(cat variables.json)" temp.json > $config_file
+    #cp $config_file temp.json
+    #jq '.variables+=$variables' --argjson variables "$(cat variables.json)" temp.json > $config_file
+
+    # Merge config files
+    cp config.json temp.json
+    jq -s '.[0] * .[1]' temp.json $config_file > config.json
 }
 
 blocks="vnet.json jumpbox.json cycle-prereqs-managed-identity.json cycle-install-server-managed-identity.json cycle-cli-local.json cycle-cli-jumpbox.json beegfs-cluster.json"
+
 for block in $blocks; do
     echo "initializing config for $block"
     init_config $azhpc_dir/experimental/blocks/$block
 done
 
-sed -i "s/#projectstore#/$locker/g" cycle-install-server-managed-identity.json
+# Update locker name
+sed -i "s/#projectstore#/$locker/g" config.json
+
+# Init cycle config file
 init_config $azhpc_dir/examples/cc_beegfs/pbscycle.json
-init_config $azhpc_dir/examples/cc_beegfs/slurmcycle.json
+
+# Concatenate install array into a single one
+jq -s '[.[].install[]]' $blocks > install.json
+
+# Replace the install array into the final config file
+items=$(cat install.json)
+jq '.install=$items' --argjson items "$items" config.json > temp.json
+cp temp.json config.json
+
+# Merge variables file into config file
+cp config.json temp.json
+jq '.variables+=$variables' --argjson variables "$(cat variables.json)" temp.json > config.json
