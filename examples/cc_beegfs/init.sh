280a90c239f64c7fdbfeed06795e8a6812512a20
Xavier Pillons <xpillons@microsoft.com>
2020-06-17 11:07:18 +0200

    don't set projectstore in the init

--- examples/cc_beegfs/init.sh
+++ examples/cc_beegfs/init.sh
@@ -3,11 +3,11 @@ block_dir=$azhpc_dir/experimental/blocks
 AZHPC_CONFIG=config.json
 AZHPC_VARIABLES=variables.json
 
-uuid_str="$(cat /proc/sys/kernel/random/uuid | tr -d '\n-' | tr '[:upper:]' '[:lower:]' | cut -c 1-6)"
-locker="locker$uuid_str"
+#uuid_str="$(cat /proc/sys/kernel/random/uuid | tr -d '\n-' | tr '[:upper:]' '[:lower:]' | cut -c 1-6)"
+#locker="locker$uuid_str"
 
-jq '.variables.projectstore=$locker' --arg locker $locker $AZHPC_VARIABLES > temp.json
-cp temp.json $AZHPC_VARIABLES
+#jq '.variables.projectstore=$locker' --arg locker $locker $AZHPC_VARIABLES > temp.json
+#cp temp.json $AZHPC_VARIABLES
 
 blocks="$block_dir/vnet.json $block_dir/jumpbox.json $block_dir/cycle-install-server-managed-identity.json $block_dir/cycle-cli-local.json $block_dir/cycle-cli-jumpbox.json $block_dir/beegfs-cluster.json $azhpc_dir/examples/cc_beegfs/pbscycle.json"
 
@@ -20,4 +20,5 @@ prereqs="$block_dir/cycle-prereqs-managed-identity.json"
 $azhpc_dir/init-and-merge.sh $prereqs prereqs.json $AZHPC_VARIABLES
 
 # Update locker name
+locker=$(jq -r '.variables.projectstore' $AZHPC_VARIABLES)
 sed -i "s/#projectstore#/$locker/g" $AZHPC_CONFIG
