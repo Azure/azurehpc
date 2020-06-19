b9ad0ba3e181de1cf805de48bf162c6844f8c915
Narjit Chadha <narjit.chadha@microsoft.com>
2020-06-19 16:26:11 -0500

    Addition of scripts directory to cc_glusterfs2

---
+++ experimental/cc_glusterfs2/scripts/glusterfs_pool.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+systemctl start glusterd
+systemctl status glusterd
---
+++ experimental/cc_glusterfs2/scripts/glusterfs_pool_master.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+HOSTLIST=$1
+
+systemctl start glusterd
+systemctl status glusterd
+
+   for host in $HOSTLIST
+   do 
+      gluster peer probe $host
+   done
