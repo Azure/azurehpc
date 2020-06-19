d8e352c78b56d1ac3f3a8bf90632011bb23551b0
Xavier Pillons <xpillons@microsoft.com>
2020-06-19 10:01:54 +0200

    improved cycle integration

---
+++ scripts/azhpc4cycle.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# This utility script contains helpers functions called in the azurehpc to CycleCloud integration
+helper=${1,,}
+
+if [ ! -e $CYCLECLOUD_HOME/bin/jetpack ]; then
+    echo "Not running in a CycleCloud environment exiting"
+else
+    echo "Running in a CycleCloud environment"
+fi
+
+enable_metada_access()
+{
+    # Enable METADATA SERVICE access if blocked. This is the case with CycleCloud 7.x by default
+    # Delete all rules regarding 169.254.169.254
+    prevent_metadata_access=$(jetpack config cyclecloud.node.prevent_metadata_access)
+    echo "cyclecloud.node.prevent_metadata_access=$prevent_metadata_access"
+    echo "Allow Metadata Service access"
+    echo "Dumping IPTABLES"
+    iptables -L
+    rule=$(iptables -S | grep -E 169.254.169.254 | tail -n1)
+    while [ -n "$rule" ]; do
+        delete_rule=$(sed 's/-A/-D/g' <<< $(echo $rule))
+        iptables $delete_rule
+        rule=$(iptables -S | grep -E 169.254.169.254 | tail -n1)
+    done
+    echo "Dumping IPTABLES"
+    iptables -L
+}
+
+# Disabling jetpack converge can only be done thru a cron as cluster-init scripts are executed before the crontab is updated with the converge entry
+# If converge is enabled, add a cron to run every minute until the converge entry in the crontab is removed
+disable_jetpack_converge()
+{
+    # Remove Jetpack converge from the crontab
+    maintenance_converge=$(jetpack config cyclecloud.maintenance_converge.enabled)
+    maintenance_converge=${maintenance_converge,,}
+    echo "cyclecloud.maintenance_converge.enabled=$maintenance_converge"
+    if [ "$maintenance_converge" == "true" ]; then
+        # Check if converge is in the crontab and if so remove it, if not add a cron entry to check it every minute
+        converge=$(crontab -l | grep "jetpack converge")
+        if [ -n "$converge" ]; then
+            echo "Dump crontab"
+            crontab -l
+            echo "Remove Jetpack converge from crontab"
+            crontab -l | grep -v "jetpack converge" | crontab -
+            echo "Remove our crontab entry"
+            crontab -l | grep -v "disable_jetpack_converge" | crontab -
+            echo "Dump crontab"
+            crontab -l
+        else
+            echo "*/1 * * * * $0 disable_jetpack_converge >> /opt/cycle/jetpack/logs/azhpc4cycle.log 2>&1" | crontab -
+        fi
+    fi
+}
+
+disable_fail2ban()
+{
+    # Disable fail2ban
+    echo "Disabling fail2ban"
+    systemctl stop fail2ban
+    systemctl disable fail2ban
+}
+
+fix_pbs_limits()
+{
+    # Fix PBS limits issue
+    if [ -e /opt/pbs/lib/init.d/limits.pbs_mom ]; then
+        echo "Fixing limits.pbs_mom"
+        sed -i "s/^if /#if /g" /opt/pbs/lib/init.d/limits.pbs_mom
+        sed -i "s/^fi/#fi /g" /opt/pbs/lib/init.d/limits.pbs_mom
+    fi
+}
+
+case $helper in
+    enable_metada_access)
+        enable_metada_access
+        ;;
+    disable_jetpack_converge)
+        disable_jetpack_converge
+        ;;
+    disable_fail2ban)
+        disable_fail2ban
+        ;;
+    fix_pbs_limits)
+        fix_pbs_limits
+        ;;
+    *)
+        echo "unknown function"
+        ;;
+esac
--- scripts/cndefault.sh
+++ scripts/cndefault.sh
@@ -17,31 +17,9 @@ chmod 777 /mnt/resource
 # - Disable Fail2Ban service
 # - Fix PBS limits
 if [ -e $CYCLECLOUD_HOME/bin/jetpack ]; then
-    echo "CycleCloud detected"
-    # Enable METADATA SERVICE access if blocked. This is the case with CycleCloud 7.x by default
-    # Delete all rules regarding 169.254.169.254
-    echo "Allow Metadata Service access"
-    iptables -L
-    rule=$(iptables -S | grep -E 169.254.169.254 | tail -n1)
-    while [ -n "$rule" ]; do
-        delete_rule=$(sed 's/-A/-D/g' <<< $(echo $rule))
-        iptables $delete_rule
-        rule=$(iptables -S | grep -E 169.254.169.254 | tail -n1)
-    done
-    iptables -L
-
-    # Remove Jetpack converge from the crontab
-    echo "Remove Jetpack converge from crontab"
-    crontab -l | grep -v converge | crontab -
-
-    # Disable fail2ban
-    echo "Disable fail2ban"
-    systemctl stop fail2ban
-    systemctl disable fail2ban
-
-    # Fix PBS limits issue
-    if [ -e /opt/pbs/lib/init.d/limits.pbs_mom ]; then
-        sed -i "s/^if /#if /g" /opt/pbs/lib/init.d/limits.pbs_mom
-        sed -i "s/^fi/#fi /g" /opt/pbs/lib/init.d/limits.pbs_mom
-    fi
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    $DIR/azhpc4cycle.sh enable_metada_access
+    $DIR/azhpc4cycle.sh disable_jetpack_converge
+    $DIR/azhpc4cycle.sh disable_fail2ban
+    $DIR/azhpc4cycle.sh fix_pbs_limits
 fi
