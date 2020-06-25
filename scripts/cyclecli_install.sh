cd0190fbec426a458a1db37b8eb700c88e157b97
Xavier Pillons <xpillons@microsoft.com>
2020-06-25 12:44:10 +0200

    fix install depending on version

--- scripts/cyclecli_install.sh
+++ scripts/cyclecli_install.sh
@@ -5,14 +5,78 @@ admin_user=$2
 password=$3
 cc_version=${4-7}
 
+install_cli7()
+{
+    echo "Installing CC CLI 7"
+    echo "Getting CLI binaries..."
+    wget -q --no-check-certificate https://$fqdn/download/tools/cyclecloud-cli.zip
+    unzip -o cyclecloud-cli.zip
+    pushd cyclecloud-cli-installer/
+
+    echo "Installing CLI..."
+    ./install.sh -y
+    echo "Initializing CLI..."
+    name=$(echo $fqdn | cut -d'.' -f1)
+    echo $name
+    ~/bin/cyclecloud initialize --force --batch \
+        --name $name \
+        --url=https://$fqdn \
+        --verify-ssl=false \
+        --username=$admin_user \
+        --password="${password}"
+
+    ~/bin/cyclecloud --version
+    ~/bin/cyclecloud config list
+    popd
+    rm cyclecloud-cli.zip
+    rm -rf cyclecloud-cli-installer
+}
+
+install_cli8()
+{
+    echo "Installing CC CLI 7"
+    echo "Getting CLI binaries..."
+    wget -q --no-check-certificate https://$fqdn/static/tools/cyclecloud-cli.zip 
+    unzip -o cyclecloud-cli.zip
+    pushd cyclecloud-cli-installer/
+
+    echo "Installing CLI..."
+    # If az CLI is installed used the bundled version
+    python_path=$(az --version | grep 'Python location' | xargs | cut -d' ' -f3)
+    if [ -n "$python_path" ]; then
+        $python_path install.py -y
+    else
+        if ! rpm -q python3; then
+            sudo yum install -y python3
+        fi
+        ./install.sh -y
+    fi
+
+    echo "Initializing CLI..."
+    name=$(echo $fqdn | cut -d'.' -f1)
+    echo $name
+    ~/bin/cyclecloud initialize --force --batch \
+        --name $name \
+        --url=https://$fqdn \
+        --verify-ssl=false \
+        --username=$admin_user \
+        --password="${password}"
+
+    ~/bin/cyclecloud --version
+    ~/bin/cyclecloud config list
+    popd
+    rm cyclecloud-cli.zip
+    rm -rf cyclecloud-cli-installer
+}
+
 # Installing CycleCloud CLI
 echo "Getting CLI binaries..."
 case "$cc_version" in
     7)
-        wget -q --no-check-certificate https://$fqdn/download/tools/cyclecloud-cli.zip 
+        install_cli7 
         ;;
     8)
-        wget -q --no-check-certificate https://$fqdn/static/tools/cyclecloud-cli.zip 
+        install_cli8 
         ;;
     *)
         echo "Version $cc_version not supported"
@@ -20,34 +84,3 @@ case "$cc_version" in
         ;;
 esac
 
-unzip -o cyclecloud-cli.zip
-pushd cyclecloud-cli-installer/
-
-
-echo "Installing CLI..."
-# If az CLI is installed used the bundled version
-python_path=$(az --version | grep 'Python location' | xargs | cut -d' ' -f3)
-if [ -n "$python_path" ]; then
-    $python_path install.py -y
-else
-    if ! rpm -q python3; then
-        sudo yum install -y python3
-    fi
-    ./install.sh -y
-fi
-
-
-echo "Initializing CLI..."
-name=$(echo $fqdn | cut -d'.' -f1)
-echo $name
-~/bin/cyclecloud initialize --force --batch \
-    --name $name \
-    --url=https://$fqdn \
-    --verify-ssl=false \
-    --username=$admin_user \
-    --password="${password}"
-
-~/bin/cyclecloud config list
-popd
-rm cyclecloud-cli.zip
-rm -rf cyclecloud-cli-installer
