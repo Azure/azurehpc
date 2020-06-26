437d4261fdd01ad65acb8946860a809084958214
Davide Vanzo <davide.vanzo@microsoft.com>
2020-06-26 15:56:27 -0500

    Added support for Slurm

--- apps/ior/ior.sh
+++ apps/ior/ior.sh
@@ -4,15 +4,15 @@ SHARED_APP=${2:-/apps}
 
 source /etc/profile # so we can load modules
 
-export MODULEPATH=${SHARED_APP}/modulefiles:$MODULEPATH
 module use ${SHARED_APP}/modulefiles
 module load ior
 
 AZHPC_VMSIZE=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2018-10-01" | jq -r '.compute.vmSize')
+AZHPC_VMSIZE='standard_hb60rs'
 export AZHPC_VMSIZE=${AZHPC_VMSIZE,,}
 #echo "Running on $AZHPC_VMSIZE"
 if [ "$AZHPC_VMSIZE" = "" ]; then
-    echo "unable to retrieve VM Size - Exiting"
+    echo "Unable to retrieve VM Size - Exiting"
     exit 1
 fi
 
@@ -25,14 +25,17 @@ case "$AZHPC_VMSIZE" in
         ;;
 esac
 
-CORES=$(cat $PBS_NODEFILE | wc -l)
-NODES=$(cat $PBS_NODEFILE | sort -u)
+if [[ -n "$PBS_NODEFILE" ]]; then
+    CORES=$(cat $PBS_NODEFILE | wc -l)
+    NODES=$(cat $PBS_NODEFILE | sort -u)
+    MPI_OPTS="-np $CORES --hostfile $PBS_NODEFILE"
+fi
 
 # Throughput test (N-N)
-mpirun  -bind-to hwthread -np $CORES --hostfile $PBS_NODEFILE $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -F -r -w -t 32m -b 4G -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
+mpirun  -bind-to hwthread $MPI_OPTS $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -F -r -w -t 32m -b 4G -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
 sleep 2
 # Throughput test (N-1)
-mpirun  -bind-to hwthread -np $CORES --hostfile $PBS_NODEFILE $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -r -w -t 32m -b 4G -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
+mpirun  -bind-to hwthread $MPI_OPTS $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -r -w -t 32m -b 4G -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
 sleep 2
 # IOPS test
-mpirun -bind-to hwthread -np $CORES --hostfile $PBS_NODEFILE $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -F -r -w -t 4k -b 128M -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
+mpirun -bind-to hwthread $MPI_OPTS $IOR_BIN/ior -a POSIX -v -i 3 -m -d 1 -B -e -F -r -w -t 4k -b 128M -o ${FILESYSTEM}/test.$(date +"%Y-%m-%d_%H-%M-%S") -O summaryFormat=JSON
