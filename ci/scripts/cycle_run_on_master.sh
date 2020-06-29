e225c72eea409a35945aa713fe2cf36ab6acee82
Xavier Pillons <xpillons@microsoft.com>
2020-06-29 18:09:19 +0200

    fix missing include

--- ci/scripts/cycle_run_on_master.sh
+++ ci/scripts/cycle_run_on_master.sh
@@ -2,6 +2,9 @@
 cluster_name=$1
 cmd=$2
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$script_dir/cyclecloud_helpers.sh"
+
 echo "Retrieving master name"
 master=$(get_master_name $cluster_name)
 echo "master is $master"
