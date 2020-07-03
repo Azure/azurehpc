234d9d1c937a084e5ef7bb8b4415eb7f91c58e99
Hugo Meiland <humeilan@microsoft.com>
2020-07-03 13:38:54 +0200

    first version CC, anf and slurm

---
+++ examples/cc_anf/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+block_dir=$azhpc_dir/blocks
+AZHPC_CONFIG=config.json
+AZHPC_VARIABLES=variables.json
+
+blocks="$block_dir/vnet.json blocks/anf.json $block_dir/jumpbox.json $block_dir/cycle-install-server-managed-identity.json $block_dir/cycle-cli-local.json $block_dir/cycle-cli-jumpbox.json blocks/slurmcycle.json"
+
+# Initialize config file
+echo "{}" >$AZHPC_CONFIG
+$azhpc_dir/init-and-merge.sh "$blocks" $AZHPC_CONFIG $AZHPC_VARIABLES
+
+echo "{}" >prereqs.json
+prereqs="$block_dir/cycle-prereqs-managed-identity.json"
+$azhpc_dir/init-and-merge.sh $prereqs prereqs.json $AZHPC_VARIABLES
+
+# Update locker name
+locker=$(azhpc-get -c $AZHPC_VARIABLES variables.projectstore | cut -d '=' -f2 | xargs)
+sed -i "s/#projectstore#/$locker/g" $AZHPC_CONFIG
