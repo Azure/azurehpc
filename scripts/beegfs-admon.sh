b1cbde5c4cdf1eb7e78133a34a81a5614cb8fa3e
Cormac Garvey <cogarvey@microsoft.com>
2020-07-14 15:41:23 -0500

    Corrected BEEGFS_MGMT_HOST variable

--- scripts/beegfs-admon.sh
+++ scripts/beegfs-admon.sh
@@ -4,7 +4,8 @@ BEEGFS_MGMT_HOST=$1
 #
 yum install -y xauth
 yum install -y beegfs-admon
+yum install -y java
 
-sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_MGMT_HOST'/g' /etc/beegfs/beegfs-admon.conf
+sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$BEEGFS_MGMT_HOST'/g' /etc/beegfs/beegfs-admon.conf
 #
 systemctl start beegfs-admon.service
