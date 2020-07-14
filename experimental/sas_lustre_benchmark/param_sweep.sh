2c80aff57434d237b73b4238031a3f24d8acc40f
Paul Edwards <paul.edwards@microsoft.com>
2020-07-14 19:00:33 +0100

    Forgot to save file

--- experimental/sas_lustre_benchmark/param_sweep.sh
+++ experimental/sas_lustre_benchmark/param_sweep.sh
@@ -4,18 +4,18 @@ echo "getting rhel_iotest from SAS website"
 ./get_rhel_iotest.sh
 
 prefix=$1
-oss_instances=$1
 oss_sku=$2
-client_instances=$3
+oss_instances=$3
 client_sku=$4
+client_instances=$5
 
 nskip=0
 iskip=0
 
-for __oss_instances__ in $oss_instances; do
-    for __oss_vm_type__ in $oss_sku; do
-        for __client_vm_instances__ in $client_instances; do
-            for __client_vm_type__ in $client_sku; do
+for __oss_vm_type__ in $oss_sku; do
+    for __oss_instances__ in $oss_instances; do
+        for __client_vm_type__ in $client_sku; do
+            for __client_vm_instances__ in $client_instances; do
 
                 echo "__oss_instances__=$__oss_instances__"
                 echo "__oss_vm_type__=$__oss_vm_type__"
