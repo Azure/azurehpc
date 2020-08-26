5a87126fe4299e40eaac1213473a5ca8bdd7607a
mengruts <mengruts@gmail.com>
2020-08-26 15:29:17 +0800

    Update build_xcelium.sh

--- apps/cadence_xcelium/build_xcelium.sh
+++ apps/cadence_xcelium/build_xcelium.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# TODO: install directory
+INSTALL_DIR="/data/xcelium/"
+
+
 WORKING_DIR="/mnt/resource"
-INSTALL_DIR="/datadrive"
-CADENCE_TOOLS_BLOB="https://edatools.blob.core.windows.net/cadence/"
+CADENCE_TOOLS_BLOB="https://edarg3diag.blob.core.windows.net/edatools/Cadence"
 ISCAPE_FILE="IScape04.23-s012lnx86.t.Z"
 
 cd ${WORKING_DIR}
@@ -10,54 +13,56 @@ cd ${WORKING_DIR}
 install_required_packages()
 {
         echo "----------------------installing required packages."
-        sudo yum install ksh
-        sudo yum install mesa-libGLU
-        sudo yum install motif
-        sudo yum redhat-lsb
-        sudo yum install glibc.i686
-        sudo yum install elfutils-libelf.i686
-        sudo yum install mesa-libGL.i686
-        sudo yum install mesa-libGLU.i686
-        sudo yum install motif.i686
-        sudo yum install redhat-lsb.i686
-        sudo yum install glibc-devel.i686
-        sudo yum install libXScrnSaver.i686
-        sudo yum install libXScrnSaver.x86_64
+        sudo yum -y install ksh
+        sudo yum -y install mesa-libGLU
+        sudo yum -y install motif
+        sudo yum -y redhat-lsb
+        sudo yum -y install glibc.i686
+        sudo yum -y install elfutils-libelf.i686
+        sudo yum -y install mesa-libGL.i686
+        sudo yum -y install mesa-libGLU.i686
+        sudo yum -y install motif.i686
+        sudo yum -y install redhat-lsb.i686
+        sudo yum -y install glibc-devel.i686
+        sudo yum -y install libXScrnSaver.i686
+        sudo yum -y install libXScrnSaver.x86_64
+        sudo yum -y install java-1.8.0-openjdk
 }
 
 install_iscape()
 {
         echo "----------------------installing IScape."
-        sudo wget ${CADENCE_TOOLS_BLOB}IScape/${ISCAPE_FILE}
+        sudo wget ${CADENCE_TOOLS_BLOB}/${ISCAPE_FILE}
         sudo zcat ${ISCAPE_FILE} | sudo tar -xvf -
+}
+
+download_archive()
+{
+        echo "----------------------downloading archive."
+        cd ${WORKING_DIR}
+        sudo wget ${CADENCE_TOOLS_BLOB}/XciliumArchive.tgz
+        sudo tar -xzvf XciliumArchive.tgz
+}
+
+install_from_archive()
+{
+        echo "---------------------installing from archive."
         cd ${WORKING_DIR}/iscape.04.23-s012/bin
-}
-
-download_xcelium()
-{
-        echo "----------------------downloading Xcelium."
-        sudo wget -P /mnt/resource/xcelium ${CADENCE_TOOLS_BLOB}Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/Base_XCELIUMMAIN20.03.001_lnx86.sdx
-        sudo wget -P /mnt/resource/xcelium ${CADENCE_TOOLS_BLOB}Xcelium/root/http_sw_cadence_com_is_XCELIUM2003_lnx86_Base/ic_index.sdx
-}
-
-install_xcelium()
-{
-        echo "---------------------installing Xcelium."
-        sudo ./iscape.sh -batch majorAction=installfromarchive ArchiveDirectory=${WORKING_DIR}/xcelium/  InstallDirectory=${INSTALL_DIR}/cadence/
+        sudo ./iscape.sh -batch majorAction=installfromarchive ArchiveDirectory=${WORKING_DIR}/XceliumArchive/ InstallDirectory=${INSTALL_DIR}
 
         # generate configuration scripts
-        sudo ./iscape.sh -batch majorAction=configure InstallDirectory=${INSTALL_DIR}/cadence/
+        # no need configuration for xcelium?
+        #sudo ./iscape.sh -batch majorAction=configure InstallDirectory=${INSTALL_DIR}
 
         # complete configuration
-        sudo /bin/sh ${INSTALL_DIR}/cadence/installData/XCELIUM2003_lnx86/batch_configure.sh
-
+        #sudo /bin/sh /data/tempus/installData/SSV201_lnx86/batch_configure.sh
         # test
-        sudo ./iscape.sh -batch majorAction=test InstallDirectory=${INSTALL_DIR}/cadence/
+        sudo ./iscape.sh -batch majorAction=test InstallDirectory=${INSTALL_DIR}
 }
 
 install_required_packages
 install_iscape
-download_xcelium
-install_xcelium
+download_archive
+install_from_archive
 
 echo "-----------------------DONE."
