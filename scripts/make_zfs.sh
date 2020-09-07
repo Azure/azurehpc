f7977eded3e4f109fd4023b76982fd289673ff31
Paul Edwards <paul.m.edwards@gmail.com>
2020-09-07 16:52:51 +0100

    Script to create zfs filesystem and example

---
+++ scripts/make_zfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=$1
+mount_point=$2
+shift 2
+# storage devices are the remaining args
+
+# install ZFS
+centos_version=$(sed 's/CentOS Linux release \([[:digit:]]\.[[:digit:]]\).*/\1/' < /etc/redhat-release)
+zfs_release_rpm="http://download.zfsonlinux.org/epel/zfs-release.el${centos_version/./_}.noarch.rpm"
+yum install -y zfs_release_rpm
+yum install -y zfs
+modprobe zfs
+
+zpool create $name "$@"
+zfs set mountpoint=$mount_point $name
