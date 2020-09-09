80be4ae980b330137a931172bd2e0314e20466bd
Cormac Garvey <cogarvey@microsoft.com>
2020-09-09 14:38:01 -0500

    Updated io500 build

--- apps/io500/build_io500.sh
+++ apps/io500/build_io500.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-APP_NAME=io-500-dev
+APP_NAME=io-500-app
+APP_VERSION=io500-isc20_v4
 SHARED_APP=/apps
 MODULE_DIR=${SHARED_APP}/modulefiles
 MODULE_NAME=${APP_NAME}
@@ -9,12 +10,13 @@ INSTALL_DIR=${SHARED_APP}/${APP_NAME}
 source /etc/profile.d/modules.sh
 
 module load gcc-9.2.0
-module load mpi/openmpi-4.0.2
+module load mpi/openmpi
 
 function create_modulefile {
 mkdir -p ${MODULE_DIR}
 cat << EOF >> ${MODULE_DIR}/${MODULE_NAME}
 #%Module
+prepend-path PATH ${INSTALL_DIR};
 prepend-path PATH ${INSTALL_DIR}/bin;
 prepend-path LD_LIBRARY_PATH ${INSTALL_DIR}/lib;
 setenv IO500_INSTALL_BASE ${INSTALL_DIR};
@@ -23,9 +25,11 @@ EOF
 
 cd ${SHARED_APP}
 export CC=`which mpicc`
-git clone https://github.com/VI4IO/io-500-dev
+git clone https://github.com/VI4IO/${APP_NAME}
 cd $APP_NAME
-./utilities/prepare.sh
-./io500.sh
+git checkout tags/$APP_VERSION
+
+./prepare.sh
+make
 
 create_modulefile
