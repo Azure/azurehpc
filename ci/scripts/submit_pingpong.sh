1daf2c3cfa2e310153c871a10e8b609b3e85bdf4
Xavier Pillons <xpillons@microsoft.com>
2020-09-28 09:52:20 +0200

    debug telemetry wrapper

--- ci/scripts/submit_pingpong.sh
+++ ci/scripts/submit_pingpong.sh
@@ -14,6 +14,7 @@ make_uuid_str
 job_group=$uuid_str
 
 echo "submit job $job_group on $nodes"
+echo "AZHPC_TELEMETRY_WRAPPER=$AZHPC_TELEMETRY_WRAPPER"
 submit_job $job_group $nodes 1 "$AZHPC_TELEMETRY_WRAPPER /apps/imb-mpi/ringpingpong.sh $mpi $mode"
 
 # Wait for all jobs to be finished
