bdd852f069f0a86f8ab546dd7d13e1c11287631a
Xavier Pillons <xpillons@microsoft.com>
2020-09-29 06:37:53 +0200

    remove agent only if installed

--- experimental/io500_benchmark/hbv2_io500_impi/scripts/remove_omsagent.sh
+++ experimental/io500_benchmark/hbv2_io500_impi/scripts/remove_omsagent.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh --purge
+# Remove agent only if installed
+if [ -e /opt/microsoft/omsagent/bin/omsadmin.sh ]; then
+    wget -q https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh --purge
+fi
