e20b27faadd3178b6f2698946c0b3ab7942700d9
Xavier Pillons <xpillons@microsoft.com>
2020-09-29 11:09:42 +0200

    add metadata instances

--- scripts/startbeeond.sh
+++ scripts/startbeeond.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 hostfile=$1
 directory=${2:-/mnt/resource/beeond}
+metadata=${3:-1}
 
 if [ "$PSSH_NODENUM" = "0" ]; then
-    sudo beeond start -n $(readlink -f $hostfile) -d $directory -c /beeond
+    sudo beeond start -n $(readlink -f $hostfile) -m $metadata -d $directory -c /beeond
 fi
