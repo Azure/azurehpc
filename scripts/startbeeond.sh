1d62f80ca1406012040566503524aaaa6bf4800b
Xavier Pillons <xpillons@microsoft.com>
2020-09-29 13:29:18 +0200

    use PDSH to start beeond

--- scripts/startbeeond.sh
+++ scripts/startbeeond.sh
@@ -4,5 +4,5 @@ directory=${2:-/mnt/resource/beeond}
 metadata=${3:-1}
 
 if [ "$PSSH_NODENUM" = "0" ]; then
-    sudo beeond start -q -F -n $(readlink -f $hostfile) -m $metadata -d $directory -c /beeond
+    sudo beeond start -q -F -P -n $(readlink -f $hostfile) -m $metadata -d $directory -c /beeond
 fi
