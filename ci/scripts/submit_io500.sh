04261459d901e490752be06f3fbf832b34769d91
Xavier Pillons <xpillons@microsoft.com>
2020-09-30 09:55:09 +0200

    config file and FS root as params

--- ci/scripts/submit_io500.sh
+++ ci/scripts/submit_io500.sh
@@ -14,10 +14,12 @@ job_group=$uuid_str
 
 echo "submit job $job_group on $nodes"
 echo "AZHPC_TELEMETRY_WRAPPER=$AZHPC_TELEMETRY_WRAPPER"
-submit_job $job_group $nodes $ppn "$AZHPC_TELEMETRY_WRAPPER /apps/io500/io500_impi_beeond.pbs"
+submit_job $job_group $nodes $ppn "$AZHPC_TELEMETRY_WRAPPER /apps/io500/io500_impi_beeond.pbs /beeond /apps/io500/config-io500.ini"
 
 # Wait for all jobs to be finished
 wait_alljobs $job_group
+
+# TODO : Add data collection and push them in blobs
 
 # Check job status, if any failed it will exit with an error code
 check_jobstatus $job_group
