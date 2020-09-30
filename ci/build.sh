05dc00e35c6899f6def94792c3c5ef8d622db06a
Xavier Pillons <xpillons@microsoft.com>
2020-09-30 10:00:50 +0200

    only upload logs if account is set

--- ci/build.sh
+++ ci/build.sh
@@ -111,25 +111,28 @@ if [[ "$return_code" -ne "0" ]] || [[ "$show_logs" == "true" ]]; then
         #cat $tmp_dir/install/*.log
         grep -A10 "\[FAILURE\]" $tmp_dir/install/*.log
 
-        echo "===================="
-        echo "Upload logs in blobs"
-        echo "===================="
-        echo ""
-        # in case of errors, upload the logs into blobs
-        echo "upload $tmp_dir into blobs"
-        blob="$SYSTEM_DEFINITIONNAME/$SYSTEM_JOBIDENTIFIER/$BUILD_BUILDNUMBER"
-        account="$AZHPC_LOG_ACCOUNT"
-        container="pipelines"
-        saskey=$( \
-            az storage container generate-sas \
-            --account-name $account \
-            --name $container \
-            --permissions "rlw" \
-            --start $(date --utc -d "-2 hours" +%Y-%m-%dT%H:%M:%SZ) \
-            --expiry $(date --utc -d "+1 hour" +%Y-%m-%dT%H:%M:%SZ) \
-            --output tsv
-        )
-        azcopy cp "$tmp_dir" "https://$account.blob.core.windows.net/$container/$blob?$saskey" --recursive=true
+        # If a logging storage account is set, upload logs into blobs
+        if [ -n "$AZHPC_LOG_ACCOUNT"]; then
+            echo "===================="
+            echo "Upload logs in blobs"
+            echo "===================="
+            echo ""
+            # in case of errors, upload the logs into blobs
+            echo "upload $tmp_dir into blobs"
+            blob="$SYSTEM_DEFINITIONNAME/$SYSTEM_JOBIDENTIFIER/$BUILD_BUILDNUMBER"
+            account="$AZHPC_LOG_ACCOUNT"
+            container="pipelines"
+            saskey=$( \
+                az storage container generate-sas \
+                --account-name $account \
+                --name $container \
+                --permissions "rlw" \
+                --start $(date --utc -d "-2 hours" +%Y-%m-%dT%H:%M:%SZ) \
+                --expiry $(date --utc -d "+1 hour" +%Y-%m-%dT%H:%M:%SZ) \
+                --output tsv
+            )
+            azcopy cp "$tmp_dir" "https://$account.blob.core.windows.net/$container/$blob?$saskey" --recursive=true
+        fi
     fi
     if [ "$return_code" -ne "0" ]; then
         exit $return_code
