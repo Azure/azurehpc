f5894efa909b87e9ff6105903a25ef4792b1dde2
Jon Shelley <jshelley.menan@gmail.com>
2020-09-30 16:55:34 -0600

    updated azinstall for ubuntu

--- scripts/install-nfsserver.sh
+++ scripts/install-nfsserver.sh
@@ -5,26 +5,38 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-yum -y install epel-release
-yum -y install nfs-utils nfs-utils-lib
+# Check to see which OS this is running on. 
+os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | sed -e 's/^"//' -e 's/"$//')
+os_maj_ver=$(cat /etc/os-release | grep "^VERSION_ID\=" | cut -d'=' -f 2 | sed -e 's/^"//' -e 's/"$//')
+echo "OS Release: $os_release"
+echo "OS Major Version: $os_maj_ver"
+if [ "$os_release" == "centos" ];then
+    yum -y install epel-release
+    yum -y install nfs-utils nfs-utils-lib
+    # Start the services
+    systemctl enable rpcbind
+    systemctl enable nfs-server
+    systemctl enable nfs-lock
+    systemctl enable nfs-idmap
+    systemctl enable nfs
+
+    systemctl start rpcbind
+    systemctl start nfs-server
+    systemctl start nfs-lock
+    systemctl start nfs-idmap
+    systemctl start nfs
+elif [ "$os_release" == "ubuntu" ];then
+    apt install nfs-kernel-server -y
+    apt install lsscsi -y
+    systemctl enable nfs-kernel-server
+    systemctl start nfs-kernel-server
+fi
 
 # Shares
 NFS_APPS=$NFS_MOUNT_POINT/apps
 NFS_DATA=$NFS_MOUNT_POINT/data
 NFS_HOME=$NFS_MOUNT_POINT/home
 NFS_SCRATCH=/mnt/resource/scratch
-
-systemctl enable rpcbind
-systemctl enable nfs-server
-systemctl enable nfs-lock
-systemctl enable nfs-idmap
-systemctl enable nfs
-
-systemctl start rpcbind
-systemctl start nfs-server
-systemctl start nfs-lock
-systemctl start nfs-idmap
-systemctl start nfs
 
 ######################################
 # Create shares and exports
@@ -33,6 +45,10 @@ mkdir -p $NFS_APPS
 mkdir -p $NFS_DATA
 mkdir -p $NFS_HOME
 mkdir -p $NFS_SCRATCH
+chown nobody:nogroup $NFS_APPS
+chown nobody:nogroup $NFS_DATA
+chown nobody:nogroup $NFS_HOME
+chown nobody:nogroup $NFS_SCRATCH
 chmod 777 $NFS_APPS
 chmod 777 $NFS_DATA
 chmod 777 $NFS_HOME
@@ -60,6 +76,10 @@ replace="s/#RPCNFSDCOUNT=16/RPCNFSDCOUNT=$nfs_proc/g"
 sed -i -e "$replace" /etc/sysconfig/nfs
 grep RPCNFSDCOUNT /etc/sysconfig/nfs
 
-systemctl restart nfs-server
+if [ "$os_release" == "centos" ];then
+    systemctl restart nfs-server
+elif [ "$os_release" == "ubuntu" ];then
+    systemctl restart nfs-kernel-server
+fi
 
 df -h
