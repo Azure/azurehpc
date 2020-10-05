2b98254a0f35a71b43e7f640f422cc3d8672d459
Xavier Pillons <xpillons@microsoft.com>
2020-10-05 05:54:00 +0200

    use the latest version of io500 for SC20

--- apps/io500/setup_directories_beeond.sh
+++ apps/io500/setup_directories_beeond.sh
@@ -19,29 +19,24 @@ function setup_directories {
         ts="$timestamp" || ts="io500"
   # directory where the data will be stored
   workdir=$(get_ini_global_param datadir $PWD/datafiles)/$ts
-  io500_workdir=$workdir-scr
+  io500_workdir=$workdir
   [ $(get_ini_global_param timestamp-resultdir True) != "False" ] &&
         ts="$timestamp" || ts="io500"
   # the directory where the output results will be kept
   resultdir=$(get_ini_global_param resultdir $PWD/results)/$ts
-  io500_result_dir=$resultdir-scr
+  io500_result_dir=$resultdir
 
-  mkdir -p $workdir-{scr,app} $resultdir-{scr,app}
-  mkdir -p $workdir-{scr,app}/{ior_easy,ior_hard,mdt_easy,mdt_hard}
+  mkdir -p $workdir $resultdir
+  mkdir -p $workdir/{ior_easy,ior_hard,mdt_easy,mdt_hard}
 
   # for ior_easy.
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_EASY_CHUNKSIZE --numtargets=$IOR_EASY_STRIPE $workdir-scr/ior_easy
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_EASY_CHUNKSIZE --numtargets=$IOR_EASY_STRIPE $workdir-app/ior_easy
+  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_EASY_CHUNKSIZE --numtargets=$IOR_EASY_STRIPE $workdir/ior_easy
 
   # stripe across all OSTs for ior_hard, 256k chunksize
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_HARD_CHUNKSIZE --numtargets=$IOR_HARD_STRIPE $workdir-scr/ior_hard
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_HARD_CHUNKSIZE --numtargets=$IOR_HARD_STRIPE $workdir-app/ior_hard
+  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=$IOR_HARD_CHUNKSIZE --numtargets=$IOR_HARD_STRIPE $workdir/ior_hard
 
   # turn off striping and use small chunks for mdtest
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir-scr/mdt_easy
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir-scr/mdt_hard
-
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir-app/mdt_easy
-  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir-app/mdt_hard
+  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir/mdt_easy
+  sudo beegfs-ctl --mount=/beeond --setpattern --chunksize=64k --numtargets=1 $workdir/mdt_hard
 
 }
