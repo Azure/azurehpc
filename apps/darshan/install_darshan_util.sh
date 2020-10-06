b8d01dd580d36fff9f9f0ff82536b7fd6dc1dc3b
Xavier Pillons <xpillons@microsoft.com>
2020-10-06 15:19:43 +0200

    single yum install

--- apps/darshan/install_darshan_util.sh
+++ apps/darshan/install_darshan_util.sh
@@ -6,17 +6,4 @@ source $SPACK_SETUP_ENV
 
 spack install darshan-util%gcc@9.2.0
 
-sudo yum install -y xauth
-
-sudo yum install -y texlive
-sudo yum install -y texlive-epstopdf
-sudo yum install -y gnuplot
-sudo yum install -y texlive-lastpage
-sudo yum install -y texlive-subfigure
-sudo yum install -y texlive-multirow
-sudo yum install -y texlive-threeparttable
-sudo yum install -y perl-Pod-LaTeX
-sudo yum install -y perl-HTML-Parser
-sudo yum install -y ghostscript
-
-sudo yum install -y evince
+sudo yum install -y xauth texlive texlive-epstopdf gnuplot texlive-lastpage texlive-subfigure texlive-multirow texlive-threeparttable perl-Pod-LaTeX perl-HTML-Parser ghostscript evince
