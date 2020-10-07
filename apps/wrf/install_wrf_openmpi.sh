7ffb1e5fc74fe1a711443d8bacc286d22bc3197c
Xavier Pillons <xpillons@microsoft.com>
2020-10-07 09:52:15 +0200

    add more logs

--- apps/wrf/install_wrf_openmpi.sh
+++ apps/wrf/install_wrf_openmpi.sh
@@ -29,28 +29,36 @@ module use ${SHARED_APP}/modulefiles
 module load spack/spack
 source $SPACK_SETUP_ENV
 
+echo "spack install"
 spack install  netcdf-fortran+mpi ^hdf5+fortran %gcc@9.2.0 ^openmpi@${OPENMPI_VER}
 
+echo "get WRF source"
 mkdir -p ${APP_DIR}
 cd ${APP_DIR}
 wget -q https://github.com/wrf-model/WRF/archive/v${APP_VERSION}.tar.gz
 tar xvf v${APP_VERSION}.tar.gz
 
+echo "spack load"
 spack load netcdf-fortran^openmpi
 spack load hdf5^openmpi
 spack load perl
+echo "module load"
 module load mpi/openmpi-${OPENMPI_VER}
 module load gcc-9.2.0
 
 export HDF5=$(spack location -i hdf5^openmpi)
+echo "HDF5=$HDF5"
 export NETCDF=$(spack location -i netcdf-fortran^openmpi)
+echo "HDF5=$NETCDF"
 
 NETCDF_C=$(spack location -i netcdf-c^openmpi)
+echo "NETCDF_C=$NETCDF_C"
 ln -sf $NETCDF_C/include/* $NETCDF/include/
 ln -sf $NETCDF_C/lib/* $NETCDF/lib/
 ln -sf $NETCDF_C/lib/pkgconfig/* $NETCDF/lib/pkgconfig
 
 cd WRF-${APP_VERSION}
+echo "apply patch"
 patch -p0 < ${APPS_WRF_DIR}/WRFV4.0-rsl-8digit.patch
 
 ./configure << EOF
