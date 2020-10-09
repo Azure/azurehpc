65b9ef4e592491f2aa346aa54ef66078db29bf7e
Jon Shelley <jshelley.menan@gmail.com>
2020-10-09 13:51:11 -0600

    fixed issue with waagent name between linux distros

--- scripts/disableagent.sh
+++ scripts/disableagent.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-ifconfig eth1 down
-systemctl stop waagent
+#ifconfig eth1 down
+
+# Check to see which OS this is running on. 
+os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | sed -e 's/^"//' -e 's/"$//')
+os_maj_ver=$(cat /etc/os-release | grep "^VERSION_ID\=" | cut -d'=' -f 2 | sed -e 's/^"//' -e 's/"$//')
+echo "OS Release: $os_release"
+echo "OS Major Version: $os_maj_ver"
+
+if [ "$os_release" == "centos" ];then
+    systemctl stop waagent
+elif [ "$os_release" == "ubuntu" ];then
+    systemctl stop walinuxagent
+fi
--- scripts/pbshooks/stop_waagent.py
+++ scripts/pbshooks/stop_waagent.py
@@ -4,12 +4,26 @@ import fcntl
 import struct
 import subprocess
 
+# Determine which linux distro you are using
+os.distro="centos"
+process = subprocess.Popen(['cat', '/etc/os-release'], stdout=subprocess.PIPE)
+out, err = process.communicate()
+lines = out.split("\n")
+for line in lines:
+    if line[:3] == "ID=":
+        os_distro = line.split("=")[1].replace('"',"")
+
+print os_distro
+waagent = "waagent"
+if os_distro == "ubuntu":
+    waagent = "walinuxagent"
+
 e = pbs.event()
 if e.type == pbs.EXECJOB_BEGIN:
     pbs.logmsg(pbs.EVENT_DEBUG, "EXECJOB_BEGIN event")
     pbs.logmsg(pbs.EVENT_DEBUG, "Stop Waagent")
     # If found do the following
-    process = subprocess.Popen(['/bin/systemctl', 'stop', 'waagent'], stdout=subprocess.PIPE)
+    process = subprocess.Popen(['/bin/systemctl', 'stop', waagent], stdout=subprocess.PIPE)
     out, err = process.communicate()
     pbs.logmsg(pbs.EVENT_DEBUG, "stdout: %s" % out)
     pbs.logmsg(pbs.EVENT_DEBUG, "stderr: %s" % err)
@@ -17,7 +31,7 @@ elif e.type == pbs.EXECJOB_END:
     pbs.logmsg(pbs.EVENT_DEBUG, "EXECJOB_END event")
     pbs.logmsg(pbs.EVENT_DEBUG, "Start Waagent")
     # If found do the following
-    process = subprocess.Popen(['/bin/systemctl', 'start', 'waagent'], stdout=subprocess.PIPE)
+    process = subprocess.Popen(['/bin/systemctl', 'start', waagent], stdout=subprocess.PIPE)
     out, err = process.communicate()
     pbs.logmsg(pbs.EVENT_DEBUG, "stdout: %s" % out)
     pbs.logmsg(pbs.EVENT_DEBUG, "stderr: %s" % err)
