06f438ecac37dbe84c484b39c7532775f2e78e24
Xavier Pillons <xpillons@microsoft.com>
2020-10-28 16:07:35 +0100

    add more comments

--- scripts/beegfsc.sh
+++ scripts/beegfsc.sh
@@ -9,7 +9,7 @@ yum install -y beegfs-client beegfs-helperd beegfs-utils
 sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
 sed -i 's/^tuneFileCacheType.*/tuneFileCacheType = '$BEEGFS_CACHE'/g' /etc/beegfs/beegfs-client.conf
 
-# Use OFED only if Infiniband is present
+# Use OFED only if Infiniband is present, so you can use an HPC image on a non IB machine
 ibv_devinfo
 if [ $? == 0 ]; then
     if [ -d "/usr/src/ofa_kernel/default/include" ]; then
