19ed5bf608040617bce7c4942cd5068fe21fa05e
Xavier Pillons <xpillons@microsoft.com>
2020-10-28 16:48:23 +0100

    fix repo names

--- scripts/update_kernel.sh
+++ scripts/update_kernel.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 RELEASE=$(cat /etc/redhat-release | cut -d' ' -f4)
 echo $RELEASE
-yum --releasever=$RELEASE --disablerepo=openlogic install -y kernel kernel-tools kernel-headers kernel-devel
+repo=updates
+status=$(yum repoinfo updates-openlogic | grep enabled)
+if [ "$status" != "" ]; then
+    repo=updates-openlogic
+fi
+echo "repo to install kernel headers is $repo"
+yum install -y --disablerepo=* --enablerepo=$repo --releasever=$RELEASE kernel kernel-tools kernel-headers kernel-devel
+
