ea5042a8a1c86f1671fb91c5af1228551acee8ce
Paul Edwards <paul.m.edwards@gmail.com>
2020-11-11 15:35:07 +0000

    Step included in marker
    This allows multiple steps to run the same script

--- pyazhpc/azinstall.py
+++ pyazhpc/azinstall.py
@@ -113,7 +113,7 @@ fi
     if sudo:
         cmdline = "sudo " + cmdline
 
-    marker = f"marker-\"'$(hostname)'\"-{targetscript[:targetscript.rfind('.')]}"
+    marker = f"marker-\"'$(hostname)'\"-{step:02}-{targetscript[:targetscript.rfind('.')]}"
 
     content += f"pssh -p {pssh_threads} -t 0 -i -h hostlists/tags/$tag \"cd {tmpdir}; test -f {marker} && echo 'script already run' || ( {cmdline} && touch {marker} ) \" >> {logfile} 2>&1\n"
 
