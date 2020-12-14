903b8df73f32f4b658a3a80ca9c7d2ffcc7e9747
Jerry Morey <24438151+themorey@users.noreply.github.com>
2020-12-14 15:37:58 -0500

    mods to install on RHEL7
    Resource agents are installed by default in CentOS but not RHEL.  The easiest way to install in RHEL is via the CentOS base repo (REF: https://clusterlabs.org/quickstart-redhat.html).  This repo has been hardcoded to release version 7 as the standard variable $releasever reports "7Server" for RHEL, whereas CentOS is just "7."
    
    The firewalld service also has to be disabled on RHEL to enable lnet to work properly.
    
    Lastly, added a check for "/mnt/resource" before trying to unmount it to prevent errors (NOTE:  RHEL mounts the local SSD to /mnt not /mnt/resource)

--- scripts/lfspkgs.sh
+++ scripts/lfspkgs.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-yum -y install lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests || exit 1
+# Use CentOS repo to install resource-agents (RHEL does not install them by default)
+if  grep -qF "Red Hat" /etc/redhat-release ; then
+    cat << EOF >> /etc/yum.repos.d/centos.repo
+[centos-7-base]
+name=CentOS-7 - Base
+mirrorlist=http://mirrorlist.centos.org/?release=7&arch=\$basearch&repo=os
+#baseurl='http://mirror.centos.org/centos/7/os/\$basearch/'
+enabled=1
+gpgcheck=1
+gpgkey=http://ftp.heanet.ie/pub/centos/7/os/x86_64/RPM-GPG-KEY-CentOS-7
+EOF
+    systemctl stop firewalld.service
+    systemctl disable firewalld.service
+fi
+
+yum -y install resource-agents dkms lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests || exit 1
 
 sed -i 's/ResourceDisk\.Format=y/ResourceDisk.Format=n/g' /etc/waagent.conf
 
@@ -12,4 +27,6 @@ if [ -f "/etc/systemd/system/temp-disk-swapfile.service" ]; then
     systemctl stop temp-disk-swapfile.service
 fi
 
-umount /mnt/resource
+if [ -d /mnt/resource ]; then
+    umount /mnt/resource
+fi
