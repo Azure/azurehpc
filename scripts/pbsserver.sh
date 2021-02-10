256be3b7977466414995e06baa2235b88bec3be7
Xavier Pillons <xpillons@microsoft.com>
2021-02-10 19:25:01 +0100

    add retry on the server install

--- scripts/pbsserver.sh
+++ scripts/pbsserver.sh
@@ -23,10 +23,10 @@ if [ "$(rpm -qa $rpm)" = "" ];then
     yum install -y $rpm_list
     systemctl enable pbs
     systemctl start pbs
-    /opt/pbs/bin/qmgr -c "s s managers += ${admin_user}@*"
-    /opt/pbs/bin/qmgr -c 's s flatuid=t'
-    /opt/pbs/bin/qmgr -c 's s job_history_enable=t'
-    /opt/pbs/bin/qmgr -c 'c r pool_name type=string,flag=h'
+    retry /opt/pbs/bin/qmgr -c "s s managers += ${admin_user}@*"
+    retry /opt/pbs/bin/qmgr -c 's s flatuid=t'
+    retry /opt/pbs/bin/qmgr -c 's s job_history_enable=t'
+    retry /opt/pbs/bin/qmgr -c 'c r pool_name type=string,flag=h'
 
     # Update the sched_config file to schedule jobs that request pool_name
     sed -i "s/^resources: \"ncpus,/resources: \"ncpus, pool_name,/g" /var/spool/pbs/sched_priv/sched_config
