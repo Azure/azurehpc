f023cfbddc528a7b94d7272aed9f6168167ed87f
Xavier Pillons <xpillons@microsoft.com>
2021-02-10 18:58:11 +0100

    first centos81 and OpenPBS20 support

--- scripts/pbsdownload.sh
+++ scripts/pbsdownload.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$script_dir/azhpc-library.sh"
 
-filename=pbspro_19.1.3.centos_7.zip
+read_os
+case "$os_maj_ver" in
+    7)
+        filename=pbspro_19.1.3.centos_7.zip
+        url=https://github.com/PBSPro/pbspro/releases/download/v19.1.3/$filename
+    ;;
+    8)
+        filename=openpbs_20.0.1.centos_8.zip
+        url=https://github.com/openpbs/openpbs/releases/download/v20.0.1/$filename
+    ;;
+esac
 
 if [ ! -f "$filename" ];then
-    wget -q https://github.com/PBSPro/pbspro/releases/download/v19.1.3/$filename
+    wget -q $url
     unzip $filename
 fi
 
--- scripts/pbsserver.sh
+++ scripts/pbsserver.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 set -e
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$script_dir/azhpc-library.sh"
+
+$script_dir/pbsdownload.sh
+
+read_os
+case "$os_maj_ver" in
+    7)
+        rpm_list="pbspro_19.1.3.centos_7/pbspro-server-19.1.3-0.x86_64.rpm"
+        rpm="pbspro-server"
+    ;;
+    8)
+        rpm_list="openpbs_20.0.1.centos_8/openpbs-server-20.0.1-0.x86_64.rpm"
+        rpm="openpbs-server"
+    ;;
+esac
 
 admin_user=$(whoami)
 
-if [ "$(rpm -qa pbspro-server)" = "" ];then
-    yum install -y pbspro-server-19.1.3-0.x86_64.rpm
+if [ "$(rpm -qa $rpm)" = "" ];then
+    yum install -y $rpm_list
     systemctl enable pbs
     systemctl start pbs
     /opt/pbs/bin/qmgr -c "s s managers += ${admin_user}@*"
