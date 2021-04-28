899db72afbf5fac6718a33155fcde050c7d22fa4
Davide Vanzo <vanzod@users.noreply.github.com>
2021-04-28 08:40:31 -0500

    Corrected stack prefix extraction logic and typos

--- experimental/EasyBuild/azurempi_install.sh
+++ experimental/EasyBuild/azurempi_install.sh
@@ -5,7 +5,7 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Find stack installation prefix
 ml EasyBuild
-STACK_PREFIX=$(echo $EBROOTEASYBUILD | cut -d'/' -f-4)
+STACK_PREFIX=$(echo $EBROOTEASYBUILD | rev | cut -d'/' -f4- | rev)
 
 # Copy custom easyblocks and easyconfigs in stack prefix directory 
 cp -rv ${THIS_SCRIPT_DIR}/custom_easyblocks  ${THIS_SCRIPT_DIR}/custom_easyconfigs ${STACK_PREFIX}
@@ -16,9 +16,9 @@ EBCONF_FILE=$(eb --show-default-configfiles | grep -A1 system-level | tail -1 | 
 # Make EasyBuild aware of the new AzureMPI easyblock
 # If include-easyblocks option already exists in configure file, prepend to existing paths
 # otherwise add it right after "[config]" section marker
-grep -q 'include-easyblocks' easybuild.cfg && \
-sed -i -e "/^\(include-easyblocks = \)\(.*\)/{s//\1${STACK_PREFIX}/custom_easyblocks/azurempi.py,\2/}" easybuild.cfg || \
-sed -i -e "/^\[config\].*/a include-easyblocks = ${STACK_PREFIX}/custom_easyblocks/azurempi.py" easybuild.cfg
+grep -q 'include-easyblocks' ${EBCONF_FILE} && \
+sed -i -e "/^\(include-easyblocks = \)\(.*\)/{s//\1${STACK_PREFIX}/custom_easyblocks/azurempi.py,\2/}" ${EBCONF_FILE} || \
+sed -i -e "/^\[config\].*/a include-easyblocks = ${STACK_PREFIX}/custom_easyblocks/azurempi.py" ${EBCONF_FILE}
 
 # Instruct EasyBuild robot to search custom easyconfigs directory before easyconfigs included with EB installation
 cat << EOF >> $EBCONF_FILE
