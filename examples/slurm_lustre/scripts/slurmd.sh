16e5821f1c36e26356b605d36aff65386c247a16
vgamayunov <74012931+vgamayunov@users.noreply.github.com>
2021-09-07 15:34:11 +0100

    SLURM example with Lustre (#533)
    Initial version of a simple SLURM cluster with Lustre

---
+++ examples/slurm_lustre/scripts/slurmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yum install -y /apps/rpms/slurm-*
+
+export SLURMUSER=1002
+groupadd -g $SLURMUSER slurm
+useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+mkdir -p /var/spool/slurm
+chown slurm /var/spool/slurm
+mkdir -p /var/log/slurm
+chown slurm /var/log/slurm
+
+ln -s /apps/slurm/slurm.conf /etc/slurm/slurm.conf
+ln -s /apps/slurm/cgroup.conf /etc/slurm/cgroup.conf
+
+# Collect the actual hardware configuration of the node
+
+slurmd -C | grep $HOSTNAME > /apps/slurm/nodeconf/$HOSTNAME
+
+systemctl enable slurmd
+
+exit 0
