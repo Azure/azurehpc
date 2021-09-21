443672dc9ab88de364f528a8faf151f8600189f0
Victor Gamayunov <vgamayunov@microsoft.com>
2021-09-21 12:07:20 +0100

    minor cleanup

--- experimental/slurm_enroot/scripts/slurm_gres.sh
+++ experimental/slurm_enroot/scripts/slurm_gres.sh
@@ -6,6 +6,5 @@ echo "AutoDetect=nvml" > /etc/slurm/gres.conf
 num_gpus=$(nvidia-smi -L | wc -l)
 
 sed -i "s/$/ gres=gpu:$num_gpus/" /apps/slurm/nodeconf/$HOSTNAME
-#grep GresTypes /apps/slurm/slurm.conf || echo "GresTypes=gpu" >> /apps/slurm/slurm.conf
 
 exit 0
