e5ddd47305e353235f67166589d0b1ab3dc8df7f
Cormac Garvey <6304999+garvct@users.noreply.github.com>
2021-10-23 16:43:54 -0500

    Deleted enroot files

--- experimental/slurm_enroot/scripts/pyxis_slurmd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -v /apps/slurm/spank_pyxis.so /usr/lib64/slurm/
-chmod +x /usr/lib64/slurm/spank_pyxis.so
-
-echo Restarting SLURMD...
-systemctl restart slurmd
--- experimental/slurm_enroot/scripts/slurmnodes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat /apps/slurm/nodeconf/* | sort > /apps/slurm/nodes.conf
-partition=compute
-
-echo "PartitionName=${partition} Nodes=ALL Default=YES MaxTime=INFINITE State=UP" > /apps/slurm/partitions.conf
-
-systemctl restart slurmctld
