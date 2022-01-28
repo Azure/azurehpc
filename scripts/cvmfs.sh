11f434a6b2964894dcc0da2c896a44c6c1507d9f
hugo meiland <hugo.meiland@microsoft.com>
2022-01-28 09:25:23 +0100

    adding script for cvmfs and eessi stack

---
+++ scripts/cvmfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+sudo yum install -y cvmfs
+sudo yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
+
+sudo bash -c "echo 'CVMFS_CLIENT_PROFILE="single"' > /etc/cvmfs/default.local"
+sudo bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
+
+sudo cvmfs_config setup
