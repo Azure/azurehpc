8e7b564291745307acd2931df062d11e706d2938
Cormac Garvey <6304999+garvct@users.noreply.github.com>
2022-06-07 12:40:56 -0500

    Support deployment from Bastion

--- pyazhpc/azconfig.py
+++ pyazhpc/azconfig.py
@@ -32,7 +32,7 @@ class ConfigFile:
         if install_from:
             if self.read_value(f"resources.{install_from}.public_ip", False):
                 dest = azutil.get_fqdn(self.read_value("resource_group"), f"{install_from}_pip")
-            elif self.read_value(f"vnet.gateway.name", False):
+            elif self.read_value(f"vnet.gateway.name", False) or not self.read_value(f"resources.{install_from}.public_ip", False):
                 dest = azutil.get_vm_private_ip(self.read_value("resource_group"), install_from)
         log.debug(f"install_from destination : {dest}")
         return dest
