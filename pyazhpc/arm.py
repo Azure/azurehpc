449d3190239b9ad45b8528f11dde503959d2ea94
hugo meiland <hugo.meiland@microsoft.com>
2022-07-05 12:12:13 +0200

    add support for rocky plan

--- pyazhpc/arm.py
+++ pyazhpc/arm.py
@@ -836,7 +836,7 @@ class ArmTemplate:
 
             # Add support for cyclecloud/azhpc plan
             plan = ""
-            if ros[0] == "azurecyclecloud" or ros[0] == "azhpc" or ros[0] == "almalinux":
+            if ros[0] == "azurecyclecloud" or ros[0] == "azhpc" or ros[0] == "almalinux" or ros[0] == "erockyenterprisesoftwarefoundationinc1653071250513":
                 plan = self.__helper_arm_create_plan(rimage)
             
             if rephemeralosdisk == True:
