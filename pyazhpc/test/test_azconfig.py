fc173aba4589fcec6761a5be861bdf76acb60ae3
Jesse Lopez <lopezjesse@microsoft.com>
2022-09-15 15:50:23 +0000

    Revert "Changes to test to test variable expansion"
    This reverts commit a8595e097acec5076b9e79b2443d779c99ae3334.

--- pyazhpc/test/test_azconfig.py
+++ pyazhpc/test/test_azconfig.py
@@ -2,17 +2,15 @@ import unittest
 
 import azconfig
 
-
 class TestConfigFile(unittest.TestCase):
 
     def setUp(self):
         self.config = azconfig.ConfigFile()
         self.config.open("test/test_config_file.json")
-        self.config_preprocessed = self.config.preprocess()
 
     def test_read_simple_value(self):
         self.assertEqual(self.config.read_value("int_value"), 42)
-
+    
     def test_read_boolean_value(self):
         self.assertEqual(self.config.read_value("bool_value"), True)
 
@@ -22,25 +20,8 @@ class TestConfigFile(unittest.TestCase):
     def test_replace_double_curly_braces(self):
         self.assertEqual(self.config.read_value("double_curly_braces"), "simple_variable=42")
 
-    # Before preprocessing, config fields using variable name definitions are not expanded
-    def test_replace_in_dict_key_before_preprocessing(self):
-        self.assertEqual(
-            self.config.read_value("resources")["variables.resource_name"],
-            "bar"
-        )
-
-    def test_replace_in_dict_value_before_preprocessing(self):
-        self.assertEqual(
-            self.config.read_value("resources")["resource-name"],
-            "variables.resource_type"
-        )
-
-    # After preprocessing, config fields using variable name definitions are expanded
-    def test_replace_in_dict_key_after_preprocessing(self):
-        self.assertEqual(self.config_preprocessed["resources"]["foo"], "bar")
-
-    def test_replace_in_dict_value_after_preprocessing(self):
-        self.assertEqual(self.config_preprocessed["resources"]["resource-name"], "resource-type")
+    def test_replace_in_dict_key(self):
+        self.assertEqual(self.config.read_value("resources.foo"), "bar")
 
 if __name__ == "__main__":
     unittest.main()
