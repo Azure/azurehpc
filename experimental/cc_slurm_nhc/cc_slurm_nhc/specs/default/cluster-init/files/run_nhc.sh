c60a3a37e7fbdca399ba7cfba021532c37ce3d90
Cormac Garvey <6304999+garvct@users.noreply.github.com>
2023-01-13 10:41:48 -0600

    Added check for exlusive node in run_nhc.sh epilog

--- experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/files/run_nhc.sh
+++ experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/files/run_nhc.sh
@@ -5,9 +5,34 @@ function set_detached_mode() {
    sudo sed -i "s/DETACHED_MODE.*/DETACHED_MODE=${TARGET_MODE}/g" /etc/default/nhc
 }
 
+function exclusive_node() {
+BASE_DIR="/sys/fs/cgroup/memory/slurm"
+cntu=0
+for dir in ${BASE_DIR}/uid_*
+do
+    cntu=$((cntu+1))
+    if [[ $cntu -gt 1 ]]; then
+       return 1
+    fi
+    cntj=0
+    for job in ${dir}/job_*
+    do
+        cntj=$((cntj+1))
+        if [[ $cntj -gt 1 ]]; then
+           return 1
+        fi
+    done
+done
+}
+
+exclusive_node
+exclusive_node_rc=$?
+
 set_detached_mode 0
-sudo /usr/sbin/nhc
-NHC_RC=$?
+if [ $exclusive_node_rc -eq 0 ]; then
+   sudo /usr/sbin/nhc
+   NHC_RC=$?
+fi
 set_detached_mode 1
 
 exit ${NHC_RC}
