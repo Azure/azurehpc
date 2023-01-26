4f2bf36a88b6913ecbce7a2b68fbfc543dcea2e5
Cormac Garvey <6304999+garvct@users.noreply.github.com>
2023-01-25 20:05:49 -0600

    Corrected pid extraction

--- experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/files/wait_for_nhc.sh
+++ experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/files/wait_for_nhc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pid=`ps -ef | grep -v kill_nhc | grep nhc | tr -s ' ' | cut -d ' ' -f2 | head -n 1`
+pid=`ps -ef | grep -v grep | grep /usr/sbin/nhc | tr -s ' ' | cut -d ' ' -f2 | head -n 1`
 
 while ps -p $pid > /dev/null 2>&1
 do
