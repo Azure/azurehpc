d81c3b927026503c940ca483f17a3236a0e5b16c
Cormac Garvey <6304999+garvct@users.noreply.github.com>
2023-06-28 10:57:56 -0500

    Correct if struct

--- experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/scripts/004_topo_graph.sh
+++ experimental/cc_slurm_nhc/cc_slurm_nhc/specs/default/cluster-init/scripts/004_topo_graph.sh
@@ -2,7 +2,7 @@
 
 if ! [ -d /opt/microsoft/ncv4 ]; then
    mkdir /opt/microsoft/ncv4
-endif
+fi
 
 cp $CYCLECLOUD_SPEC_PATH/files/nc48v4-topo.xml /opt/microsoft/ncv4
 cp $CYCLECLOUD_SPEC_PATH/files/nc48v4-graph.xml /opt/microsoft/ncv4
