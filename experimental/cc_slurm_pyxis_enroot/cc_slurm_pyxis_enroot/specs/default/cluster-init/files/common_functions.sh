19a7bf8964bbb49e05a539a68ff0e6c16a80b36c
Davide Vanzo <davide.vanzo@microsoft.com>
2023-12-19 13:48:58 -0600

    CC v8.4+ compatibility

--- experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/common_functions.sh
+++ experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/common_functions.sh
@@ -1,5 +1,5 @@
 # Set of common functions used across scripts
 
 function is_slurm_controller() {
-   systemctl list-units --full -all | grep -q slurmctld
+    ls /lib/systemd/system/ | grep -q slurmctld
 }
