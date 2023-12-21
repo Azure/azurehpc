7a7e85b6c238583b14599eafc953be95c47e97a6
Davide Vanzo <davide.vanzo@microsoft.com>
2023-12-21 14:30:55 -0600

    Install pyxis and enroot from local packages

--- experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/install_enroot.sh
+++ experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/install_enroot.sh
@@ -2,24 +2,16 @@
 
 source $CYCLECLOUD_SPEC_PATH/files/common_functions.sh
 
-ENROOT_VERSION_FULL=${1:-3.4.1-1}
-ENROOT_VERSION=${ENROOT_VERSION_FULL%-*}
+ENROOT_VERSION='3.4.1-1'
 
 # Install enroot RPM packages on compute nodes
 if ! is_slurm_controller; then
 
-    cd /tmp
-
     # Install NVIDIA enroot
-    curl -fSsL -O https://github.com/NVIDIA/enroot/releases/download/v${ENROOT_VERSION}/enroot_${ENROOT_VERSION_FULL}_amd64.deb
-    curl -fSsL -O https://github.com/NVIDIA/enroot/releases/download/v${ENROOT_VERSION}/enroot+caps_${ENROOT_VERSION_FULL}_amd64.deb
-    apt install -y ./enroot_${ENROOT_VERSION_FULL}_amd64.deb
-    apt install -y ./enroot+caps_${ENROOT_VERSION_FULL}_amd64.deb
+    apt install -y $CYCLECLOUD_SPEC_PATH/files/enroot_${ENROOT_VERSION}_amd64.deb
+    apt install -y $CYCLECLOUD_SPEC_PATH/files/enroot+caps_${ENROOT_VERSION}_amd64.deb
 
     # Install NVIDIA container support
     apt-get install -y libnvidia-container1 libnvidia-container-tools
-
-    rm -f /tmp/enroot_${ENROOT_VERSION_FULL}_amd64.deb
-    rm -f /tmp/enroot+caps_${ENROOT_VERSION_FULL}_amd64.deb
 
 fi
