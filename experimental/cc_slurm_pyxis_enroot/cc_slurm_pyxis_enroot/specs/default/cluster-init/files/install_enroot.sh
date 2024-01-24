c52f2770c86eb2f2fcacec5e8dadcf511e5260ee
Davide Vanzo <davide.vanzo@microsoft.com>
2024-01-24 14:41:59 -0600

    Upgrade to latest release and ignore hold

--- experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/install_enroot.sh
+++ experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/install_enroot.sh
@@ -3,6 +3,7 @@
 source $CYCLECLOUD_SPEC_PATH/files/common_functions.sh
 
 ENROOT_VERSION='3.4.1-1'
+LIBNVIDIA_VERSION='1.14.4-1'
 
 # Install enroot RPM packages on compute nodes
 if is_compute_node; then
@@ -12,6 +13,6 @@ if is_compute_node; then
     apt install -y $CYCLECLOUD_SPEC_PATH/files/enroot+caps_${ENROOT_VERSION}_amd64.deb
 
     # Install NVIDIA container support
-    apt-get install -y libnvidia-container=1.14.3-1 libnvidia-container-tools=1.14.3-1
+    apt-get install -y --allow-change-held-packages libnvidia-container=${LIBNVIDIA_VERSION} libnvidia-container-tools=${LIBNVIDIA_VERSION}
 
 fi
