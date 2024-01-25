af05e253636fca8ad108c14bcbbb53f5e0424655
Davide Vanzo <davide.vanzo@microsoft.com>
2024-01-25 09:53:45 -0600

    Cannot differentiate between compute and login nodes

--- experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/common_functions.sh
+++ experimental/cc_slurm_pyxis_enroot/cc_slurm_pyxis_enroot/specs/default/cluster-init/files/common_functions.sh
@@ -4,10 +4,6 @@ function is_slurm_controller() {
     ls /lib/systemd/system/ | grep -q slurmctld
 }
 
-function is_login_node() {
-    ! (ls /lib/systemd/system/ | grep -q slurm)
-}
-
 function is_compute_node() {
-	! is_slurm_controller && ! is_login_node
+	! is_slurm_controller
 }
