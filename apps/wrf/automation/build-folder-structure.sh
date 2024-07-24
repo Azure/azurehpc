07ee0a6ccdbd7504c96d6e974d77f3e979f38a1f
Marcus Gaspar <marcusga@microsoft.com>
2024-07-24 19:22:26 -0300

    fix geog files

--- apps/wrf/automation/build-folder-structure.sh
+++ apps/wrf/automation/build-folder-structure.sh
@@ -20,6 +20,9 @@ mkdir -p ${WRFDAT}/tables/namelist/
 echo "creating ${WRFDAT}/gfs_data/"
 mkdir -p ${WRFDAT}/gfs_data/
 
+echo "creating ${WRFDAT}/geog/"
+mkdir -p ${WRFDAT}/geog/
+
 echo "creating /apps/scripts/"
 mkdir -p /apps/scripts/
 
@@ -39,4 +42,7 @@ ln -s /apps/hbv3/wps-openmpi/WPS-4.1/* /data/wrfdata/tables/wps/
 
 echo "creating links to WRF files"
 ln -s /apps/hbv3/wrf-openmpi/WRF-4.1.5/run/* /data/wrfdata/tables/wrf/
+
+echo "Moving geog files"
+#mv albedo_modis albedo_modis.tar.bz2 greenfrac_fpar_modis greenfrac_fpar_modis.tar.bz2 lai_modis_10m lai_modis_10m.tar.bz2 maxsnowalb_modis maxsnowalb_modis.tar.bz2 modis_landuse_20class_30s_with_lakes modis_landuse_20class_30s_with_lakes.tar.bz2 orogwd_10m orogwd_10m.tar.bz2 soiltemp_1deg soiltemp_1deg.tar.bz2 soiltype_bot_30s soiltype_bot_30s.tar.bz2 soiltype_top_30s soiltype_top_30s.tar.bz2 topo_10m topo_10m.tar.bz2 topo_2m topo_2m.tar.bz2 topo_30s topo_30s.tar.bz2 topo_gmted2010_30s topo_gmted2010_30s.tar.bz2 geog 
 
